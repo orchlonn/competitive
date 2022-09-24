fbf99043b7c76bc0d4eb8e4e6a29db5857e23919
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-09-23 22:10:12 -0700

    syntax improve

--- js/nerge_sorted_array_88.js
+++ js/nerge_sorted_array_88.js
@@ -5,17 +5,15 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-    var i = m + n - 1, m = m - 1, n = n - 1;
-     while(n >= 0){
-         if(nums1[m] > nums2[n] && m >= 0){
-             nums1[i] = nums1[m];
-             m--;
-         } else{
-             nums1[i] = nums2[n];
-             n--;
-         }
-         i--;
-     }
-     console.log(nums1);
- };
+var merge = function(nums1, m, nums2, n) {
+   var i = m + n - 1, m = m - 1, n = n - 1;
+    while(n >= 0){
+        if(nums1[m] > nums2[n] && m >= 0){
+            nums1[i--] = nums1[m--];
+        } else{
+            nums1[i--] = nums2[n--];
+        }
+
+    }
+    console.log(nums1);
+};
