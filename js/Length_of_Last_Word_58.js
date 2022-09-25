7f4a0f622b8f4b92b5ea1fabaf31240999ef79e0
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-09-25 01:14:31 -0700

    58th problem solved

---
+++ js/Length_of_Last_Word_58.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {
+
+    // string dotorh vgnvvdee space eer tasalj awj bna.
+    
+    // 1. trim function bol vgiin ehend bolon tugsguld bga hooson zaig ustgana.
+    // 2. split function bol vgiig space eer tasalj awaad array bolgoj bga
+    const arr = s.trim().split(" ");
+
+    // suuliin vgiin urtiig olood return hiij bga.
+    return arr[arr.length - 1].length;
+};
