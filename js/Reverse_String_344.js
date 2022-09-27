41f75fbd329ab0be23d4501440dc91a22ca615de
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-09-26 22:32:56 -0700

    added time complexity and explanation on 344th problem

--- js/Reverse_String_344.js
+++ js/Reverse_String_344.js
@@ -6,9 +6,16 @@
     //  pointer ashiglaj bodson
     var left = 0, right = s.length - 1, tmp ="";
     while(left < right){
+
+        // tmp dotor tvr hadgalah huwisagchaa hiisen
         tmp = s[left];
+
+        // hamgiin ehnii vsgend hamgiin tugsuliin vsgiig hiigeed left ee nemj bna 
         s[left++] = s[right];
+
+        // hamgiin tugsguliin vsgend tmp buyu left index tei vsgvvdiig hiigeed baruunaasaa negiig hasch bna
         s[right--] = tmp;
     }
+    // time complexity: 1loop = O(n + m)
     return s;
 };
