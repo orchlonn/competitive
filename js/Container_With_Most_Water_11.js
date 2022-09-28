380d7ba96d1572e2ab701dc1b08c2ffcd552c1ca
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-09-27 23:55:30 -0700

    print lines are removed and orginazed well

--- js/Container_With_Most_Water_11.js
+++ js/Container_With_Most_Water_11.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
  var maxArea = function(height) {
+     
     var  j = 1; maxArea = 0;
     var tmp = j;
     
     for(var i = 0; i < height.length - 1; i++) {
-        console.log("print 8 times")
+
         tmp = j;
         j++;
-        console.log(tmp);
+
         while(tmp < height.length - i - 1){
             
            if(height[i] >= height[tmp]){
               maxArea = height[tmp] * (tmp - i);
-               console.log(`max area 1 : ${maxArea}`);
             } else { 
               maxArea = height[i] * (tmp - i);  
-            console.log(`max area 2 : ${i}`);
             }
+
             j++;
         }
     }
