d71b2516bb3e879cfb7ea2d884c648e93ee6339c
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-10-10 23:38:20 -0700

    485th problem in progress

---
+++ js/Max_Consecutive_Ones_485.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMaxConsecutiveOnes = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] == 1) { 
+            
+        }
+    }
+
+    return max;
+};
