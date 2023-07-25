0dcec9d3f251fc3726ce21d91ced30774f154fc5
orchlonn <orchllnz@gmail.com>
2023-07-25 22:59:53 +0800

    solved blind 75 problem

---
+++ pypup/blind_75/array/Maximum_Product_Subarray.py
@@ -0,0 +1,15 @@
+def maximum_product(nums): 
+    res = max(nums)
+
+    curMin, curMax = 1,1
+    for n in nums:
+        if n == 0: 
+            curMin, curMax = 1,1
+            continue
+            
+        tmp = curMax * n
+        curMax = max(n * curMax, n * curMin, n)
+        curMin = min(tmp, n * curMin, n)
+        res = max(res,curMax)
+
+    return res
