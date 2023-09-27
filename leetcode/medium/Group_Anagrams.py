fe2fb75203f791c8feb9e789f5e3e197286ae7d1
orchlonn <orchllnz@gmail.com>
2023-09-27 08:00:45 -0700

    solved a medium problem

---
+++ leetcode/medium/Group_Anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    groups = defaultdict(list)
+
+    # `s` is the order of vowels in strs 
+    for s in strs:
+        # sorting the `s` and put the value to the key
+        key = "".join(sorted(s))
+        # the sorted `key` is the key of the group and put the unsorted `s` to that group's value
+        groups[key].append(s)
+        
+    return groups.values()
+
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
