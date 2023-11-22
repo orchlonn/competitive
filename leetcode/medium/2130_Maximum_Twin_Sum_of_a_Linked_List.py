c800cefd7ad5a468a32b13349a38980a85840208
orchlonn <orchllnz@gmail.com>
2023-11-21 16:24:11 -0800

    2130th problem solved by 2nd solution

--- leetcode/medium/2130_Maximum_Twin_Sum_of_a_Linked_List.py
+++ leetcode/medium/2130_Maximum_Twin_Sum_of_a_Linked_List.py
@@ -24,4 +24,23 @@ class Solution: # Solution - 1
         return res
     
     
-      
+class Solution: # Solution - 2
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        slow, fast = head, head
+        prev = None
+
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        res = 0
+        while slow and prev:   
+            res = max(res, prev.val + slow.val)
+            
+            slow = slow.next
+            prev = prev.next
+        
+        return res
