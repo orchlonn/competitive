4b42e404a53bce259c85ac16e49a8b5f114585fe
orchlonn <orchllnz@gmail.com>
2023-11-22 16:39:16 -0800

    82th problem solved

---
+++ leetcode/medium/82_Remove_Duplicates_from_Sorted_List_II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                left.next = head.next
+            else:
+                left = left.next
+            
+            head = head.next
+
+        
+        return dummy.next
