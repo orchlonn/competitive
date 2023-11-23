8bf5fb67538222e617707992de3096234031f9c2
orchlonn <orchllnz@gmail.com>
2023-11-22 23:17:04 -0800

    1721 problem solved

---
+++ leetcode/medium/1721_Swapping_Nodes_in_a_Linked_List.py
@@ -0,0 +1,15 @@
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        cur = head
+        for i in range(k - 1):
+            cur = cur.next
+
+        left = cur
+        right = head
+
+        while cur.next:
+            cur = cur.next
+            right = right.next
+        
+        left.val, right.val = right.val, left.val
+        return head
