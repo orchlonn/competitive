b0c4e38d3daab907afefcf995643b3788103f705
orchlonn <orchllnz@gmail.com>
2023-11-26 00:09:06 -0800

    solved same problem again for remembering the architecture

--- leetcode/easy/234_Palindrome_Linked_List.py
+++ leetcode/easy/234_Palindrome_Linked_List.py
@@ -5,34 +5,24 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Edge case #1
-        if head.next == None:
-            return True
-
-        # create the pointers
         slow, fast = head, head
-        
-        # find the 2nd half
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
 
-        # assign the current value and reverse the 2nd half
-        cur = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
         prev = None
         while slow:
-            nxtPoint = slow.next
+            temp = slow.next
             slow.next = prev
             prev = slow
-            slow = nxtPoint
-        
-        # checks every element from 1st and 2nd half
+            slow = temp
+
         while prev:
-            if prev.val != cur.val:
+            if head.val != prev.val:
                 return False
-                break
-            else:
-                prev = prev.next
-                cur = cur.next
-        
-        return True
+            prev = prev.next
+            head = head.next
+
+        return true
+                
