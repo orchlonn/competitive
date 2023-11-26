621b1cab8a4dfcb8b19cdbcd436f7234d41d3322
orchlonn <orchllnz@gmail.com>
2023-11-25 23:56:29 -0800

    solved same problem again for remembering the architecture

--- leetcode/medium/1721_Swapping_Nodes_in_a_Linked_List.py
+++ leetcode/medium/1721_Swapping_Nodes_in_a_Linked_List.py
@@ -3,13 +3,14 @@ class Solution:
         cur = head
         for i in range(k - 1):
             cur = cur.next
-
+        
         left = cur
         right = head
-
-        while cur.next:
+        
+        while cur and cur.next:
             cur = cur.next
             right = right.next
-        
+            
         left.val, right.val = right.val, left.val
+
         return head
