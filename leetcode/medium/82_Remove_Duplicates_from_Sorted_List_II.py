e30b68ae29ca281e4795825e6768fe5e3b91295d
orchlonn <orchllnz@gmail.com>
2023-11-28 08:57:59 -0800

    solved same problem again for remembering the architecture

--- leetcode/medium/82_Remove_Duplicates_from_Sorted_List_II.py
+++ leetcode/medium/82_Remove_Duplicates_from_Sorted_List_II.py
@@ -3,14 +3,14 @@ class Solution:
         dummy = ListNode(0, head)
         left = dummy
 
-        while head:
-            if head.next and head.val == head.next.val:
-                while head.next and head.val == head.next.val:
+        while head and head.next:
+            if head and head.next and head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
                     head = head.next
-                left.next = head.next
+                left.next = left.next
             else:
                 left = left.next
             
-            head = head.next
-        
-        return dummy.next 
+            right = right.next
+
+        return dummy.next
