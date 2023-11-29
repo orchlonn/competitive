164238304aa6c9c6700d6d91e36512e78630950c
orchlonn <orchllnz@gmail.com>
2023-11-29 08:59:22 -0800

    solved same problem again for remembering the architecture

--- leetcode/medium/328_Odd_Even_Linked_List.py
+++ leetcode/medium/328_Odd_Even_Linked_List.py
@@ -1,9 +1,9 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Base case #1
+        # Edge case #1
         if head == None:
             return head
-            
+        
         odd, even = head, head.next
         evenHead = even
 
@@ -12,7 +12,6 @@ class Solution:
             odd = odd.next
             even.next = odd.next
             even = even.next
-        
-        odd.next = evenHead
 
+        odd.next = evenHead
         return head
