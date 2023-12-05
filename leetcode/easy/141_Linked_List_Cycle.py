65f41bf79e0cbf795b97f1d3a2febbed03203d93
orchlonn <orchllnz@gmail.com>
2023-12-04 21:46:05 -0800

    solved same problem again for remembering the structure

--- leetcode/easy/141_Linked_List_Cycle.py
+++ leetcode/easy/141_Linked_List_Cycle.py
@@ -1,17 +1,13 @@
 # Definition for singly-linked list.
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slower = head
-        faster = head
+        slower, faster = head, head
 
         while faster and faster.next:
             faster = faster.next.next
             slower = slower.next
             if slower == faster:
                 return True
-            
-            
-        return False
-            
         
-
+        return False
+        
