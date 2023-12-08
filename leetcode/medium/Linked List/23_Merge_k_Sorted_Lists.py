2ed218b57e89ec62b51799be78fda93a78c1b129
orchlonn <orchllnz@gmail.com>
2023-12-08 09:59:35 -0800

    solved same problem again for remembering the structure

--- leetcode/medium/Linked List/23_Merge_k_Sorted_Lists.py
+++ leetcode/medium/Linked List/23_Merge_k_Sorted_Lists.py
@@ -5,30 +5,31 @@ class Solution:
             return None
         
         while len(lists) > 1:
-            mergeList = []
+            mergedLists = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if len(lists) > (i + 1) else None
-                mergeList.append(self.mergeTwoLists(l1,l2))
-            lists = mergeList
-        
+                mergedLists.append(self.mergeTwoLists(l1, l2))
+            lists = mergedLists
         return lists[0]
+                
     
     def mergeTwoLists(self, l1, l2):
         dummy = ListNode()
         cur = dummy
-
+        
         while l1 and l2:
-            if l1.val > l2.val:
-                cur.next = l2
-                l2 = l2.next
-            else:
+            if l2.val > l1.val:
                 cur.next = l1
                 l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
             cur = cur.next
         
         if l1:
             cur.next = l1
         else:
             cur.next = l2
+        
         return dummy.next
