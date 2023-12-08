d7ce2097d0e04199d20270085681b674a5faecd3
orchlonn <orchllnz@gmail.com>
2023-12-07 18:56:13 -0800

    solved same problem again for remembering the structure

--- leetcode/medium/Linked List/23_Merge_k_Sorted_Lists.py
+++ leetcode/medium/Linked List/23_Merge_k_Sorted_Lists.py
@@ -1,34 +1,34 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Edge case 1
         if not lists or len(lists) == 0:
             return None
         
         while len(lists) > 1:
-            mergedList = []
-            for i in range(0, range(lists), 2):
+            mergeList = []
+            for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedList.append(mergeTwoLists(l1, l2))
-            lists = mergedList
+                l2 = lists[i + 1] if len(lists) > (i + 1) else None
+                mergeList.append(self.mergeTwoLists(l1,l2))
+            lists = mergeList
+        
         return lists[0]
     
     def mergeTwoLists(self, l1, l2):
         dummy = ListNode()
         cur = dummy
-        
+
         while l1 and l2:
-            if l2.val > l1.val:
-                cur.next = l1
-                l1 = l1.next
-            else:
+            if l1.val > l2.val:
                 cur.next = l2
                 l2 = l2.next
-            
+            else:
+                cur.next = l1
+                l1 = l1.next
             cur = cur.next
-
+        
         if l1:
             cur.next = l1
         else:
             cur.next = l2
-        
         return dummy.next
