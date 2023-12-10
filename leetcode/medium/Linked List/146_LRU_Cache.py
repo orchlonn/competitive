07549a2e7f28532a5483c09b9edd724a90d78935
orchlonn <orchllnz@gmail.com>
2023-12-09 16:25:02 -0800

    solved same preblem for remembering the structure

--- leetcode/medium/Linked List/146_LRU_Cache.py
+++ leetcode/medium/Linked List/146_LRU_Cache.py
@@ -1,22 +1,22 @@
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
-        self.next = self.prev = None
+        self.prev =  self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
-
-        # left is LRU, right is for recent used
+        
+        # left - > LRU, right -> recent used
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-
+        
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
@@ -32,11 +32,12 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
+        self.cache[key] = Node(key, value)        
         self.insert(self.cache[key])
-
+        
         if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+        
 
