263e3f1c832c931e2c2ea93d03fd9476c65ceb0f
orchlonn <orchllnz@gmail.com>
2023-12-25 14:42:22 -0800

    designed stack using aray in JavaScript

---
+++ design and implementing/design_stack_II.js
@@ -0,0 +1,17 @@
+class Stack {
+  constructor() {
+    this.arr = [];
+  }
+
+  peek = () => this.arr[this.arr.length - 1];
+
+  push(value) {
+    this.arr.push(value);
+    return this;
+  }
+
+  pop() {
+    this.arr.length != 0 ? this.arr.pop() : null;
+    return this;
+  }
+}
