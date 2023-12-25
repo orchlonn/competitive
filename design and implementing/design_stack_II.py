5738bb891e19f9e3398903816d9adcb088fd70c3
orchlonn <orchllnz@gmail.com>
2023-12-25 14:37:59 -0800

    designed stack using aray in python

---
+++ design and implementing/design_stack_II.py
@@ -0,0 +1,19 @@
+# designing stack using array
+class Stack:
+    def __init__(self):
+        self.arr = []
+        
+    def peek():
+        return self.arr[len(self.arr) - 1]
+    
+    def push(value):
+        self.arr.push(value)
+        return self.arr
+        
+    def pop():
+        if len(arr) != 0:
+            self.arr.pop()
+            return self.arr        
+        else:
+            return None
+        
