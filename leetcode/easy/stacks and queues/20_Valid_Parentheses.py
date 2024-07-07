77c2b722622707fe61b486dcd3e7cd07819d6a7d
orchlonn <orchllnz@gmail.com>
2024-07-07 22:13:35 +0800

    20th problem solved

--- leetcode/easy/stacks and queues/20_Valid_Parentheses.py
+++ leetcode/easy/stacks and queues/20_Valid_Parentheses.py
@@ -2,17 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         matching = {"(": ")", "[": "]", "{": "}"}
-        
+
         for c in s:
-            if c in matching:  # if c is an opening bracket
+            if c in matching:
                 stack.append(c)
             else:
                 if not stack:
                     return False
-                
+
                 prev_opening = stack.pop()
                 if matching[prev_opening] != c:
                     return False
-
             
         return not stack
