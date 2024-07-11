86dacfda3b2c47351fd8df294ee00de0ef88727f
orchlonn <orchllnz@gmail.com>
2024-07-11 12:03:31 +0800

    150th problem solved

--- leetcode/medium/stacks and queues/150_Evaluate_Reverse_Polish_Notation.py
+++ leetcode/medium/stacks and queues/150_Evaluate_Reverse_Polish_Notation.py
@@ -1,40 +1,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        self.stackNumbers = []
-        self.stackOperators = []
-        operators = ['+', '-', '*', '/']
-        res = 0 
-
+        stack = []
+        
         for c in tokens:
-            if c in operators:
-                self.stackOperators.append(c)
+            if c == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif c == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
             else:
-                self.stackNumbers.append(c)
-
-            if res == 0 and self.stackOperators:
-                print(self.stackNumbers)
-                first = int(self.stackNumbers.pop())
-                second = int(self.stackNumbers.pop())
-                operator = self.stackOperators.pop()
-                # print(first, second)
-                if operator == '+':
-                    res =  second + first
-                elif operator == '-':
-                    res =  second - first
-                elif operator == '*':
-                    res =  second * first
-                else:
-                    res = int(second / first)
-            elif self.stackOperators:
-                last = int(self.stackNumbers.pop())
-                operator = self.stackOperators.pop()
-                if operator == '+':
-                    res += last
-                elif operator == '-':
-                    res -= last
-                elif operator == '*':
-                    res *= last
-                else:
-                    res = int(last / res)
-
-        return res
+                stack.append(int(c))
+        
+        return stack[0]
