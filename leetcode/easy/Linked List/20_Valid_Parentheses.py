465560037879cb02e20cd10412492b87d632b846
orchlonn <ch.orchlon@gmail.com>
2024-08-03 16:10:14 +0800

    20th problem solved

---
+++ leetcode/easy/Linked List/20_Valid_Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        matching  = {"(": ")", "[": "]", "{": "}"}
+
+        for c in s:
+            if c in matching:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+
+                prev_opening = stack.pop()
+                if matching[prev_opening] != c:
+                    return False
+
+        return not stack
