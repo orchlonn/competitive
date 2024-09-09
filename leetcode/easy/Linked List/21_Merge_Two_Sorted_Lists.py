b8bea211c2fb81a1c0507abf37add8275eba78c3
orchlonn <ch.orchlon@gmail.com>
2024-09-09 15:57:42 +0800

    21st problem solved

--- leetcode/easy/Linked List/21_Merge_Two_Sorted_Lists.py
+++ leetcode/easy/Linked List/21_Merge_Two_Sorted_Lists.py
@@ -1,21 +1,21 @@
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
+        dummy = LinkedList()
         cur = dummy
 
         while l1 and l2:
-            if l1 < l2:
+            if l1.val < l2.val:
                 cur.next = l1
                 l1 = l1.next
             else:
                 cur.next = l2
                 l2 = l2.next
-            
+
             cur = cur.next
-        
+
         if l1 != None:
             cur.next = l1
         else:
             cur.next = l2
-        
+
         return dummy.next
