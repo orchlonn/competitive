f1199fe59e9b8c87738d2e751cb929870efc5b0c
orchlonn <ch.orchlon@gmail.com>
2024-09-10 17:33:19 +0800

    solved 74th problem

--- leetcode/74_Search_a_2D_Matrix.py
+++ leetcode/74_Search_a_2D_Matrix.py
@@ -33,20 +33,20 @@ class Solution:
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-        left, right = 0, m * n - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
+        l, r = 0, n * m - 1
+
+        while l <= r:
+            mid = (l + r) // 2
             row = mid // n
             col = mid % n
             num = matrix[row][col]
-            
+
             if num == target:
                 return True
-            
-            if num < target:
-                left = mid + 1
+
+            if target > num:
+                l = mid + 1
             else:
-                right = mid - 1
+                r = mid - 1
         
         return False
