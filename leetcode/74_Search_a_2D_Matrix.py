56435d7d7c648cdef1ec3f70df1b521c242054d0
orchlonn <ch.orchlon@gmail.com>
2024-09-10 15:38:56 +0800

    implemented better solution for 74th problem

--- leetcode/74_Search_a_2D_Matrix.py
+++ leetcode/74_Search_a_2D_Matrix.py
@@ -26,3 +26,27 @@ class Solution:
                 return True
         
         return False
+
+# Solution 2:
+# time complexity: 0(logN)
+# space complexity: 0(1)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n
+            num = matrix[row][col]
+            
+            if num == target:
+                return True
+            
+            if num < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
