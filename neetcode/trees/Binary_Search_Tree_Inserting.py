ed92086a124e4baf04ea13f881eb0c48f764112d
orchlonn <ch.orchlon@gmail.com>
2024-09-16 13:24:38 +0800

    binary search tree inserting implementation

---
+++ neetcode/trees/Binary_Search_Tree_Inserting.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+# Insert a new node and return the root of the BST.
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    return root
+
