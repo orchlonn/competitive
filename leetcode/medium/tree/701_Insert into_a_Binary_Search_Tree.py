f9f8dbd016ce7f5f452291d7d2643a049d7025f8
orchlonn <ch.orchlon@gmail.com>
2024-09-22 21:38:59 +0800

    701st problem solved

--- leetcode/medium/tree/701_Insert into_a_Binary_Search_Tree.py
+++ leetcode/medium/tree/701_Insert into_a_Binary_Search_Tree.py
@@ -13,5 +13,4 @@ class Solution:
             root.right = self.insertIntoBST(root.right, val)
         elif val < root.val:
             root.left = self.insertIntoBST(root.left, val)
-
         return root
