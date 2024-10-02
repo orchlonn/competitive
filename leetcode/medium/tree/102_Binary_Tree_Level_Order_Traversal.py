174b72b50eef7e98f8b2e62fa66a130563ac6aec
orchlonn <ch.orchlon@gmail.com>
2024-10-02 11:33:11 -0700

    102nd problem solved

--- leetcode/medium/tree/102_Binary_Tree_Level_Order_Traversal.py
+++ leetcode/medium/tree/102_Binary_Tree_Level_Order_Traversal.py
@@ -1,31 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+  # Definition for a binary tree node.
+  # class TreeNode:
+  #     def __init__(self, val=0, left=None, right=None):
+  #         self.val = val
+  #         self.left = left
+  #         self.right = right
+  class Solution:
+      def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         queue = deque()
         ans = []
 
         if root:
-            queue.append(root)
-        
+          queue.append(root)
+
         level = 0
         while queue:
-            val = []
-            for i in range(len(queue)):
-                cur = queue.popleft()
-                val.append(cur.val)
+          val = []
+          for i in range(len(queue)):
+            cur = queue.popleft()
+            val.append(cur.val)
 
-                if cur.left:
-                    queue.append(cur.left)
-                if cur.right:
-                    queue.append(cur.right)
-
-            level += 1
-            ans.append(val)
+            if cur.right:
+              queue.append(cur.right)
+            if cur.left:
+              queue.append(cur.left)
+          
+          level += 1
+          ans.append(val)
 
         return ans
-            
