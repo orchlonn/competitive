8b5e303dc7b745c1ab6dd873d0be1b61364a5e34
orchlonn <ch.orchlon@gmail.com>
2024-10-14 09:50:40 -0700

    112nd problem solved

---
+++ leetcode/easy/trees/112_Path_Sum.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curr):
+          curr += node.val
+
+          if (node.left == None and node.right == None):
+            return curr == targetSum
+
+          left = dfs(node.left, curr)
+          right = dfs(node.right, curr)
+
+          print(left, ' ', right)
+          return left or right
+        
+        return dfs(root, 0)
