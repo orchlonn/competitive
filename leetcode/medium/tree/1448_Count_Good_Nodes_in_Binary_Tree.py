11d4634403a922a56ea6fffd29f11f2d4ec4b216
orchlonn <ch.orchlon@gmail.com>
2024-10-16 13:14:56 -0700

    1448th problem revised

--- leetcode/medium/tree/1448_Count_Good_Nodes_in_Binary_Tree.py
+++ leetcode/medium/tree/1448_Count_Good_Nodes_in_Binary_Tree.py
@@ -9,11 +9,12 @@ class Solution:
         def dfs(node, max_so_far):
             if not node:
                 return 0
-            
+
             left = dfs(node.left, max(max_so_far, node.val))
             right = dfs(node.right, max(max_so_far, node.val))
 
             ans = left + right
+
             if node.val >= max_so_far:
                 ans += 1
             
