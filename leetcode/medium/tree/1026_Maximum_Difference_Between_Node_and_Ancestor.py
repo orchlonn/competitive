26b29f5fdefd3529fc05e7737abf5c3cb3132571
orchlonn <ch.orchlon@gmail.com>
2024-10-18 20:32:00 -0700

    1026th problem solved

--- leetcode/medium/tree/1026_Maximum_Difference_Between_Node_and_Ancestor.py
+++ leetcode/medium/tree/1026_Maximum_Difference_Between_Node_and_Ancestor.py
@@ -3,20 +3,19 @@ class Solution:
         if not root:
             return 0
         
-        self.res = 0
+        self.ans = 0
 
         def helper(node, curr_max, curr_min):
             if not node:
-                return
-            
-            self.res = max(self.res, abs(curr_max - node.val), abs(curr_min - node.val))
+                return 
+            self.ans = max(self.ans, abs(curr_max - node.val), abs(curr_min - node.val))
 
             curr_max = max(curr_max, node.val)
             curr_min = min(curr_min, node.val)
 
             helper(node.left, curr_max, curr_min)
             helper(node.right, curr_max, curr_min)
-        
+
         helper(root, root.val, root.val)
 
-        return self.res
+        return self.ans
