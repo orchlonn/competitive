13184c39864a01572b47dcf13e877f374c319629
orchlonn <ch.orchlon@gmail.com>
2024-10-18 20:18:02 -0700

    111th problem revised

--- leetcode/easy/trees/111_Minimum_Depth_of_Binary_Tree.py
+++ leetcode/easy/trees/111_Minimum_Depth_of_Binary_Tree.py
@@ -3,26 +3,25 @@
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root:
-            return 0 
-            
+            return 0
+        
         q = collections.deque([root])
         res = 1
 
         while q:
-            qLength = len(q)
-
-            for _ in range(qLength):
+            for _ in range(len(q)):
                 node = q.popleft()
 
                 if not node:
                     continue
-                
+
                 if node.left == None and node.right == None:
                     return res
                 
                 q.append(node.left)
                 q.append(node.right)
-            
+
             res += 1
-        
+
         return -1
+                    
