d5bfe2748b9b90524ae7b99bdeb82b6b925625b6
orchlonn <ch.orchlon@gmail.com>
2024-10-22 17:38:02 -0700

    701th problem solved

--- leetcode/medium/tree/701_Insert into_a_Binary_Search_Tree.py
+++ leetcode/medium/tree/701_Insert into_a_Binary_Search_Tree.py
@@ -8,9 +8,10 @@ class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
             return TreeNode(val)
-
-        if val > root.val:
+        
+        if value > root.val:
             root.right = self.insertIntoBST(root.right, val)
-        elif val < root.val:
+        else:
             root.left = self.insertIntoBST(root.left, val)
+        
         return root
