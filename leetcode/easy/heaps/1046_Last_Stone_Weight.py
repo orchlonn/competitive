11dd650e0065a04b42c6e843e941e361f92e9b2b
orchlonn <ch.orchlon@gmail.com>
2024-10-25 15:53:20 -0700

    1046th problem solved

---
+++ leetcode/easy/heaps/1046_Last_Stone_Weight.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-stone for stone in stones]
+
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+            if first != second:
+                heapq.heappush(stones, -abs(first - second))
+
+        return -stones[0] if stones else 0
