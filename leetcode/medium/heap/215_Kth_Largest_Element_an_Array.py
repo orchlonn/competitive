08fe3b85e38d041fc292c2e24aca62705d999830
orchlonn <ch.orchlon@gmail.com>
2024-10-25 20:56:54 -0700

    2nd solution of problem 215

--- leetcode/medium/heap/215_Kth_Largest_Element_an_Array.py
+++ leetcode/medium/heap/215_Kth_Largest_Element_an_Array.py
@@ -13,3 +13,15 @@ class Solution:
         return heap[0]
 
 
+# Solution 2
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = [-num for num in nums]
+
+        heapq.heapify(heap)
+        ans = 0
+        while k > 0:
+            ans = heapq.heappop(heap)
+            k -= 1
+
+        return -ans
