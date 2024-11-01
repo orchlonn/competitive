569c910f82cc950f98771eba4eb7e1992fbf349f
orchlonn <ch.orchlon@gmail.com>
2024-11-01 16:52:51 -0700

    547th problem solved

---
+++ leetcode/medium/graphs/547_Number_of_Provinces.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        n = len(isConnected)
+
+        def dfs(node):
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+
+        # build graph
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        ans = 0
+        seen = set()
+        # cound the province
+        for i in range(n):
+            if i not in seen:
+                ans += 1
+                seen.add(i)
+                dfs(i)
+
+        return ans
