9bb648dab261dd69a42bbc01bca36d2078e737f7
orchlonn <ch.orchlon@gmail.com>
2024-11-02 10:49:00 -0700

    solved neetcode

--- neetcode/graphs/Matrix_Depth_First_Search.py
+++ neetcode/graphs/Matrix_Depth_First_Search.py
@@ -1,22 +1,28 @@
 class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
-        ROWS, COLUMNS = len(grid), len(grid[0])
-
-        def helper(grid, r, c, visit):
+        
+        def dfs(grid, r, c, visit):
+            ROWS, COLS = len(grid), len(grid[0])
+            
             if (min(r, c) < 0 
-                or r == ROWS or c == COLUMNS
-                or (r, c) in visit or grid[r][c] == 1):
-                return 0
-            if r == ROWS - 1 and c == COLUMNS - 1:
+                or (r, c) in visit
+                or c == COLS or r == ROWS
+                or grid[r][c] == 1
+               ):
+               return 0
+            
+            if (r == ROWS - 1 and c == COLS - 1):
                 return 1
             
             visit.add((r, c))
             count = 0
-            count += helper(grid, r + 1, c, visit)
-            count += helper(grid, r - 1, c, visit)
-            count += helper(grid, r, c + 1, visit)
-            count += helper(grid, r, c - 1, visit)
+            count += dfs(grid, r + 1, c, visit)
+            count += dfs(grid, r - 1, c, visit)
+            count += dfs(grid, r, c + 1, visit)
+            count += dfs(grid, r, c - 1, visit)
 
+            # backtracking 
             visit.remove((r, c))
             return count
-        return helper(grid, 0, 0, set())
+
+        return dfs(grid, 0, 0, set())
