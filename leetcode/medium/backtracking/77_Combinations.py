b497b30abea4ab746fa77688ee52fd12ffe6bf1f
orchlonn <ch.orchlon@gmail.com>
2024-11-04 14:40:08 -0800

    77th problem solved

--- leetcode/medium/backtracking/77_Combinations.py
+++ leetcode/medium/backtracking/77_Combinations.py
@@ -1,15 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def backtrack(curr, i):
-            if len(curr) == k:
-                ans.append(curr[:])
-                return
+        def backtrack(comb, i):
+            if len(comb) == k:
+                ans.append(list(comb))
+                return 
 
-            for num in range(i, nums + 1):
-                curr.append(num)
-                backtrack(curr, num + 1)
-                curr.pop()
-
+            for num in range(i, n + 1):
+                comb.append(num)
+                backtrack(comb, num + 1)
+                comb.pop()
+            
         ans = []
         backtrack([], 1)
-        return ans 
+        return ans
