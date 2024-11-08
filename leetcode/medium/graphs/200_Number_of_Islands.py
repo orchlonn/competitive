2d05594b25f9814c7594c2c40142db37ba0c8cef
orchlonn <ch.orchlon@gmail.com>
2024-11-07 17:53:18 -0800

    200th problem solved

--- leetcode/medium/graphs/200_Number_of_Islands.py
+++ leetcode/medium/graphs/200_Number_of_Islands.py
@@ -3,27 +3,28 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
+        rows, cols = len(grid), len(grid[0])        
+        seen = set()
         islands = 0
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
             if (
-                r not in range(rows)
+                r not in range(rows) 
                 or c not in range(cols)
                 or grid[r][c] == "0"
-                or (r, c) in visit
+                or (r, c) in seen
             ):
-                return
-
-            visit.add((r, c))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+                return 
+            
+            seen.add((r, c))
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
+                if grid[r][c] == "1" and (r, c) not in seen:
                     islands += 1
                     dfs(r, c)
+
         return islands
