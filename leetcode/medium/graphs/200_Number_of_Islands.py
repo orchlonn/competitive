1c21e8246776f5b6e2a28bc477792a47671f3cd4
orchlonn <ch.orchlon@gmail.com>
2025-01-05 18:25:48 -0800

    200th problem solved

--- leetcode/medium/graphs/200_Number_of_Islands.py
+++ leetcode/medium/graphs/200_Number_of_Islands.py
@@ -1,28 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
+        if not grid or grid[0]:
             return 0
-        
-        island = 0
-        ROWS, COLS = len(grid), len(grid[0])
+
+        ROWS, COLS = len(grid), leng(grid[0])
         seen = set()
+        island = 0
 
         def dfs(row, col):
-            if( row not in range(ROWS)
-                or col not in range(COLS)
-                or grid[row][col] == "0"
-                or (row, col) in seen
+            if (min(row, col) < 0 or
+                (row, col) in seen or
+                grid[row][col] == '0'
             ):
                 return
-            
-            seen.add((row, col))
+
+            seen.add(row, col)
             directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for dx, dy in directions:
                 dfs(row + dx, col + dy)
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) not in seen and grid[r][c] == "1":
+        for r in range(grid):
+            for c in range(grid[0]):
+                if ((r, c) not in seen and grid[r][c] == '1'):
                     island += 1
                     dfs(r, c)
         
