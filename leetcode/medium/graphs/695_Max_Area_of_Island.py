289fa28283bf52c6ee31eb7b60260aa4e07abb9b
orchlonn <ch.orchlon@gmail.com>
2025-01-05 20:39:06 -0800

    695-spaced repetition

--- leetcode/medium/graphs/695_Max_Area_of_Island.py
+++ leetcode/medium/graphs/695_Max_Area_of_Island.py
@@ -1,25 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        seen = set()
-        rows, cols = len(grid), len(grid[0])
-        ans = 0
+        if not grid and grid[0]:
+            return 0
 
-        def dfs(r, c):
-            if( r not in range(rows)
-                or c not in range(cols)
-                or (r, c) in seen
-                or grid[r][c] == 0
+        ROWS, COLS = len(grid), len(grid[0])
+        area = 0
+        visit = set()
+
+        def dfs(row, col):
+            if (row not in range(ROWS) or
+                col not in range(COLS) or
+                (row, col) in visit or
+                grid[row][col] == 0
             ):
                 return 0
-    
-            seen.add((r, c))
-            return (1 + dfs(r + 1, c) + 
-                        dfs(r - 1, c) + 
-                        dfs(r, c + 1) + 
-                        dfs(r, c - 1))
-
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) not in seen and grid[r][c] == 1:
-                    ans = max(ans, dfs(r, c))
-        return ans
+                
+            visit.add((row, col))
+            return (1 + dfs(row + 1, col) +
+                        dfs(row - 1, col) + 
+                        dfs(row, col + 1) + 
+                        dfs(row, col - 1))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if ((r, c) not in visit and grid[r][c] == 1):
+                    area = max(area, dfs(r, c))
+        
+        return area
