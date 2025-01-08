53037d3791637207415483dab6d8eaf05162e476
orchlonn <ch.orchlon@gmail.com>
2025-01-07 19:26:36 -0800

    1091st problem solved

--- leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
+++ leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
@@ -1,25 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        directions = [[0,1], [0,-1], [1,0], [-1, 0],
-                      [1,1], [1,-1], [-1,1], [-1, -1]]
-        q = deque([(0, 0, 1)])
-        visit = set((0, 0))
+        queue = deque([(0, 0, 1)]) # row, column, length
+        visist = set((0, 0))
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1],
+                      [1, -1], [-1, 1], [-1, -1], [1, 1]]
 
-        while q:
-            r, c, length = q.popleft()
-            if (min(r, c) < 0 or
-                max(r, c) >= N or
+        while queue:
+            r, c, length = queue.popleft()
+            if (r not in range(N) or
+                c not in range(N) or
                 grid[r][c] == 1
             ):
                 continue
-                
+            
             if r == N - 1 and c == N - 1:
                 return length
-                
-            for dr, dc in directions:
-                if ((r + dr, c + dc) not in visit):
-                    q.append((r + dr, c + dc, length + 1))
-                    visit.add((r + dr, c + dc))
+
+            for dx, dy in directions:
+                if (r + dx, c + dy) not in visit:
+                    queue.append((r + dx, c + dy, length + 1))
+                    visit.add((r + dx, c + dy))
             
-        return -1
+        return -1 
