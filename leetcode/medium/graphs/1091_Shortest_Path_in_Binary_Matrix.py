0fe24e1238114a4e6401d7ffb164c4166f96642b
orchlonn <ch.orchlon@gmail.com>
2025-01-09 15:05:33 -0800

    1091 - spaced repetition

--- leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
+++ leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
@@ -1,24 +1,24 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
-        visit = set((0, 0))
         queue = deque([(0, 0, 1)])
-
+        visit = set((0, 0))
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1],
+                      [1, 1], [-1, 1], [1, -1], [-1, -1]]
+        
         while queue:
             r, c, length = queue.popleft()
-            if (grid[r][c] == 0 or
-                r not in range(N) or
-                c not in range(N)
+            if (r not in range(N) or 
+                c not in range(N) or
+                grid[r][c] == 1
             ):
                 continue
-
             if r == N - 1 and c == N - 1:
                 return length
-
+            
             for dx, dy in directions:
-                if (r + dx, c + dy) not in visit and grid[r][c] == 1:
+                if (r + dx, c + dy) not in visit and grid[r][c] == 0:
                     visit.add((r + dx, c + dy))
                     queue.append((r + dx, c + dy, length + 1))
             
-        return -1
+        return -1 
