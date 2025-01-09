84a5fead354c0c9908fc0a6ebdbc06f7f162a531
orchlonn <ch.orchlon@gmail.com>
2025-01-08 19:18:02 -0800

    994-spaced repetition

--- leetcode/medium/graphs/994_Rotting_Oranges.py
+++ leetcode/medium/graphs/994_Rotting_Oranges.py
@@ -1,29 +1,26 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        time, fresh = 0, 0
-        q = deque()
-        
-        for r in range(rows):
-            for c in range(cols):
+        ROWS, COLS = len(grid), len(grid[0])
+        queue = deque()
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        fresh, counter = 0, 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
-                    q.append([r, c])
+                    queue.append((r, c))
         
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while q and fresh > 0:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if (min(row, col) < 0 or
-                        row not in range(rows) or
-                        col not in range(cols) or
-                        grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-                    fresh -= 1
-            time += 1
-        return time if fresh == 0 else -1
+        while queue and fresh > 0:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                for dx, dy in directions:
+                    if r + dx in range(ROWS) and c + dy in range(COLS) and grid[r + dx][c + dy] == 1:
+                        grid[r + dx][c + dy] = 2
+                        fresh -= 1
+                        queue.append((r + dx, c + dy))
+
+
+            counter += 1
+        return counter if fresh == 0 else -1
