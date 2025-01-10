fbd3551b400c02d816b16ec0456bfbeed2d80b78
orchlonn <ch.orchlon@gmail.com>
2025-01-10 14:35:47 -0800

    994-spaced repetition

--- leetcode/medium/graphs/994_Rotting_Oranges.py
+++ leetcode/medium/graphs/994_Rotting_Oranges.py
@@ -1,9 +1,9 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         queue = deque()
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        fresh, counter = 0, 0
+        cntr, fresh = 0, 0
 
         for r in range(ROWS):
             for c in range(COLS):
@@ -11,16 +11,18 @@ class Solution:
                     fresh += 1
                 if grid[r][c] == 2:
                     queue.append((r, c))
-        
+
         while queue and fresh > 0:
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dx, dy in directions:
-                    if r + dx in range(ROWS) and c + dy in range(COLS) and grid[r + dx][c + dy] == 1:
-                        grid[r + dx][c + dy] = 2
+                    row, col = r + dx, c + dy
+                    if ((row) in range(ROWS) and 
+                        (col) in range(COLS) and
+                        grid[row][col] == 1
+                    ):
                         fresh -= 1
-                        queue.append((r + dx, c + dy))
-
-
-            counter += 1
-        return counter if fresh == 0 else -1
+                        queue.append((row, col))
+                        grid[row][col] = 2
+            cntr += 1
+        return cntr if fresh == 0 else -1
