01d98e94e4cfc361d95b9c1c94285867c9059fb7
orchlonn <ch.orchlon@gmail.com>
2025-01-14 21:41:57 -0800

    547-spaced repetition

--- leetcode/medium/graphs/547_Number_of_Provinces.py
+++ leetcode/medium/graphs/547_Number_of_Provinces.py
@@ -1,26 +1,28 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        graph = defaultdict(list)
         N = len(isConnected)
         visit = set()
-        graph = defaultdict(list)
-        ans = 0
-      
+        res = 0
+        
         for i in range(N):
             for j in range(i + 1, N):
+                # checking that there is a connection between i and j
                 if isConnected[i][j]:
                     graph[i].append(j)
                     graph[j].append(i)
-                    
+        
         def dfs(node):
+            # for loop to check all the neighbours of the node
             for nei in graph[node]:
                 if nei not in visit:
                     visit.add(nei)
                     dfs(nei)
-                    
+        
         for i in range(N):
             if i not in visit:
-                ans += 1
                 visit.add(i)
+                res += 1
                 dfs(i)
-    
-        return ans
+                
+        return res
