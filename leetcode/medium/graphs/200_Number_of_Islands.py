cc7cb38cd2d465a933eed4dab1d6abf549b37c7b
orchlonn <ch.orchlon@gmail.com>
2025-01-14 21:59:11 -0800

    200-spaced repetition

--- leetcode/medium/graphs/200_Number_of_Islands.py
+++ leetcode/medium/graphs/200_Number_of_Islands.py
@@ -1,19 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         visit = set()
         island = 0
 
         def dfs(row, col):
-            if(row not in range(ROWS) or 
+            if (row not in range(ROWS) or
                 col not in range(COLS) or
                 (row, col) in visit or
                 grid[row][col] == '0'
             ):
                 return
-            
+
             visit.add((row, col))
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             for dx, dy in directions:
                 dfs(row + dx, col + dy)
         
@@ -22,5 +22,5 @@ class Solution:
                 if (r, c) not in visit and grid[r][c] == '1':
                     island += 1
                     dfs(r, c)
-
+        
         return island
