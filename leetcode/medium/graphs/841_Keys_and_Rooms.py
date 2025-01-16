d85b3b01060d44a3638b64453458e27fb3e8ae63
orchlonn <ch.orchlon@gmail.com>
2025-01-15 16:34:16 -0800

    841-spaced repetition

--- leetcode/medium/graphs/841_Keys_and_Rooms.py
+++ leetcode/medium/graphs/841_Keys_and_Rooms.py
@@ -1,11 +1,15 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        for i in range(len(rooms)):
+            graph[i] = rooms[i]
+        
         def dfs(node):
-            for neighbor in rooms[node]:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    dfs(neighbor)
-            
-        seen = {0}
+            visit.add((node))
+            for nei in graph[node]:
+                if nei not in visit:
+                    dfs(nei)
+        
+        visit = set()
         dfs(0)
-        return len(seen) == len(rooms)
+        return True if len(visit) == len(rooms) else False
