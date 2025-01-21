f1fbf7b1ec766a06c7056892a4806d3c5920e14f
orchlonn <ch.orchlon@gmail.com>
2025-01-21 10:16:52 -0800

    added more validation-200

--- leetcode/medium/graphs/200_Number_of_Islands.py
+++ leetcode/medium/graphs/200_Number_of_Islands.py
@@ -1,9 +1,9 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         visit = set()
-        island = 0
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        ans = 0
 
         def dfs(row, col):
             if (row not in range(ROWS) or
@@ -11,16 +11,17 @@ class Solution:
                 (row, col) in visit or
                 grid[row][col] == '0'
             ):
-                return
-
+                return 
+            
             visit.add((row, col))
             for dx, dy in directions:
-                dfs(row + dx, col + dy)
+                if (row + dx in range(ROWS) and col + dy in range(COLS) and grid[row + dx][col + dy] and (row + dx, col + dy) not in visit):
+                    dfs(row + dx, col + dy)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) not in visit and grid[r][c] == '1':
-                    island += 1
+                    ans += 1
                     dfs(r, c)
         
-        return island
+        return ans
