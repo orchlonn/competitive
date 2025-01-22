ed1572000555c53b78d23d071579a6ad5c089588
orchlonn <ch.orchlon@gmail.com>
2025-01-22 13:23:23 -0800

    695-spaced repetition

--- leetcode/medium/graphs/695_Max_Area_of_Island.py
+++ leetcode/medium/graphs/695_Max_Area_of_Island.py
@@ -1,18 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set()
-        res = 0
+        seen = set()
+        ans = 0
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         def dfs(row, col):
             if (row not in range(ROWS) or
                 col not in range(COLS) or
-                (row, col) in visit or
-                grid[row][col] == 0
+                (row, col) in seen or
+                grid[row][col] == 0 
             ):
                 return 0
             
-            visit.add((row, col))
+            seen.add((row, col))
             return (1 + dfs(row + 1, col) + 
                         dfs(row - 1, col) + 
                         dfs(row, col + 1) +
@@ -20,7 +21,7 @@ class Solution:
         
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) not in visit and grid[r][c] == 1:
-                    res = max(res, dfs(r, c))
-
-        return res
+                if grid[r][c] == 1 and (r, c) not in seen:
+                    ans = max(ans, dfs(r, c))
+        
+        return ans
