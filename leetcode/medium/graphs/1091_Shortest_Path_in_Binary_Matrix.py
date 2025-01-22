c2678413429476cfa12d462c6e98cd729c2ab0a3
orchlonn <ch.orchlon@gmail.com>
2025-01-22 14:54:04 -0800

    1091-spaced repetition

--- leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
+++ leetcode/medium/graphs/1091_Shortest_Path_in_Binary_Matrix.py
@@ -5,20 +5,23 @@ class Solution:
         visit = set((0, 0))
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1],
                       [1, 1], [-1, 1], [1, -1], [-1, -1]]
-        
+                      
         while queue:
             r, c, length = queue.popleft()
-            if (r not in range(N) or 
+            
+            if (r not in range(N) or
                 c not in range(N) or
                 grid[r][c] == 1
             ):
                 continue
+            
             if r == N - 1 and c == N - 1:
                 return length
             
             for dx, dy in directions:
-                if (r + dx, c + dy) not in visit and grid[r][c] == 0:
-                    visit.add((r + dx, c + dy))
-                    queue.append((r + dx, c + dy, length + 1))
-            
+                row, col = r + dx, c + dy
+                if (row, col) not in visit and grid[r][c] == 0:
+                    queue.append((row, col, length + 1))
+                    visit.add((row, col))
+        
         return -1
