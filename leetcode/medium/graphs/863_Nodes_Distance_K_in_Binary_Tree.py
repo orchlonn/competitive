474fc7fd7c9b85bd87f5269991ed91ae2f630ab4
orchlonn <ch.orchlon@gmail.com>
2025-01-24 07:31:32 -0800

    863-spaced repetition

--- leetcode/medium/graphs/863_Nodes_Distance_K_in_Binary_Tree.py
+++ leetcode/medium/graphs/863_Nodes_Distance_K_in_Binary_Tree.py
@@ -10,26 +10,24 @@ class Solution:
         def dfs(node, parent):
             if not node:
                 return
-
             node.parent = parent
             dfs(node.left, node)
             dfs(node.right, node)
-
-        dfs(root, None) 
+        
+        dfs(root, None)
         distance = 0
+        visit = {target}
         queue = deque([(target)])
-        visit = set([(target)])
 
         while queue and distance < k:
-            current_level = len(queue)  
-            for _ in range(current_level):
+            curr_len = len(queue)
+            for _ in range(curr_len):
                 node = queue.popleft()
-                for nei in [node.left, node.right, node.parent]:
-                    if nei and nei not in visit:
-                        visit.add((nei))
-                        queue.append((nei))
-
+                for neighbor in [node.left, node.right, node.parent]:
+                    if neighbor and neighbor not in visit:
+                        visit.add((neighbor))
+                        queue.append((neighbor))
+            
             distance += 1
-
         
-        return [node.val for node in queue] 
+        return [node.val for node in queue]
