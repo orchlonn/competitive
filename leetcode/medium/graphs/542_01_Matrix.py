939b95433136252c65bdcbf470d920000d2d1d59
orchlonn <ch.orchlon@gmail.com>
2025-01-25 08:37:57 -0800

    542-spaced repetition

--- leetcode/medium/graphs/542_01_Matrix.py
+++ leetcode/medium/graphs/542_01_Matrix.py
@@ -3,24 +3,25 @@ class Solution:
         def isValid(row, col):
             return row in range(ROWS) and col in range(COLS)
 
-        ROWS, COLS = len(mat), len(mat[0]) 
+        ROWS, COLS = len(mat), len(mat[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         queue = deque()
         visit = set()
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        for row in range(ROWS):
-            for col in range(COLS):
+        for row in range(len(mat)):
+            for col in range(len(mat[row])):
                 if mat[row][col] == 0:
                     queue.append((row, col, 1))
                     visit.add((row, col))
-        
+
         while queue:
             row, col, steps = queue.popleft()
             for dx, dy in directions:
-                next_row, next_col = row + dx, col + dy
-                if isValid(next_row, next_col) and (next_row, next_col) not in visit:
-                    queue.append((next_row, next_col, steps + 1))
-                    visit.add((next_row, next_col))
-                    mat[next_row][next_col] = steps
+                new_row = row + dx
+                new_col = col + dy
+                if (isValid(new_row, new_col) and (new_row, new_col) not in visit):
+                    visit.add((new_row, new_col))
+                    queue.append((new_row, new_col, steps + 1))
+                    mat[new_row][new_col] = steps
         
         return mat
