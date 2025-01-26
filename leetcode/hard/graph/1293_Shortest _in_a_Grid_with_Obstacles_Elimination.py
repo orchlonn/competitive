f719b1c07959f0c763b023aec253206139241a1d
orchlonn <ch.orchlon@gmail.com>
2025-01-26 09:34:03 -0800

    1293-spaced repetition

--- leetcode/hard/graph/1293_Shortest _in_a_Grid_with_Obstacles_Elimination.py
+++ leetcode/hard/graph/1293_Shortest _in_a_Grid_with_Obstacles_Elimination.py
@@ -2,30 +2,29 @@ class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         def isValid(row, col):
             return row in range(ROWS) and col in range(COLS)
-
+            
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set((0, 0, k))
-        queue = deque([(0, 0, k, 0)]) # row, col, steps, k
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
+        queue = deque([(0, 0, 0, k)]) # row, col, steps, remainings
+        visit = set((0, 0, k)) # row, col, remainings
+        
         while queue:
-            row, col, remain, steps = queue.popleft()
-                
+            row, col, steps, remaining = queue.popleft()
+            
+            # base case 1: 
             if row == ROWS - 1 and col == COLS - 1:
                 return steps
             
             for dx, dy in directions:
-                next_row, next_col = row + dy, col + dx
+                next_row, next_col = row + dx, col + dy
                 if isValid(next_row, next_col):
-                    if grid[next_row][next_col] == 0:
-                        if (next_row, next_col, remain) not in visit:
-                            visit.add((next_row, next_col, remain))
-                            queue.append((next_row, next_col, remain, steps + 1))
-                    # otherwise, it is an obstacle and we can only pass if we have remaining removals
-                    elif remain and (next_row, next_col, remain - 1) not in visit:
-                        visit.add((next_row, next_col, remain - 1))
-                        queue.append((next_row, next_col, remain - 1, steps + 1))
+                    if (grid[next_row][next_col] == 0 and (next_row, next_col, remaining) not in visit):
+                        queue.append((next_row, next_col, steps + 1, remaining))
+                        visit.add((next_row, next_col, remaining))
+                        
+                    elif remaining and (next_row, next_col, remaining - 1) not in visit:
+                        queue.append((next_row, next_col, steps + 1, remaining - 1))
+                        visit.add((next_row, next_col, remaining - 1))
+                        
 
         return -1
-        
-
