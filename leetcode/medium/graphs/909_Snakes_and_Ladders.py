effc3f2df296de62d5990cd7dd337bd4ac072154
orchlonn <ch.orchlon@gmail.com>
2025-02-01 08:48:03 -0800

    909-spaced repetition

--- leetcode/medium/graphs/909_Snakes_and_Ladders.py
+++ leetcode/medium/graphs/909_Snakes_and_Ladders.py
@@ -2,33 +2,32 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         length = len(board)
         board.reverse()
-
-        def intToPos(square):
-            r = (square - 1) // length
-            c = (square - 1) % length
-            if r % 2:
-                c = length - 1 - c
-
-            return [r, c]
         
-        queue = deque([(1, 0)]) #square, steps
+        def intToPos(square):
+            row = (square - 1) // length
+            col = (square - 1) % length
+            if row % 2:
+                col = length - 1 - col
+            return [row, col]    
+        
+        queue = deque([(1, 1)])
         visit = set()
-
+        
         while queue:
             square, steps = queue.popleft()
             
             for i in range(1, 7):
                 nextSquare = square + i
                 row, col = intToPos(nextSquare)
-
+                
                 if board[row][col] != -1:
                     nextSquare = board[row][col]
-
-                if nextSquare == length * length:
-                    return steps + 1
                 
-                if (nextSquare) not in visit:
+                if nextSquare == length * length:
+                    return steps
+                
+                if nextSquare not in visit:
                     visit.add((nextSquare))
                     queue.append((nextSquare, steps + 1))
-
+                    
         return -1
