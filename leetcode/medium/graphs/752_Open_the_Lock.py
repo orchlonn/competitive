0a98504f2b18ce5d9d40e0103b9b55170d4b3b55
orchlonn <ch.orchlon@gmail.com>
2025-02-02 08:43:19 -0800

    752-spaced repetition

--- leetcode/medium/graphs/752_Open_the_Lock.py
+++ leetcode/medium/graphs/752_Open_the_Lock.py
@@ -1,8 +1,8 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         if "0000" in deadends:
-            return -1 
-            
+            return -1
+
         def children(locks):
             res = []
             for i in range(4):
@@ -10,21 +10,21 @@ class Solution:
                 res.append(locks[:i] + digit + locks[i+1:])
                 digit = str((int(locks[i]) - 1 + 10) % 10)
                 res.append(locks[:i] + digit + locks[i+1:])
-            
-            return res
-            
-        q = deque([("0000", 0)])
+
+            return res          
+
+        queue = deque([("0000", 0)])
         visit = set((deadends))
-        
-        while q:
-            locks, turns = q.popleft()
-            
+
+        while queue:
+            locks, turns = queue.popleft()
+
             if locks == target:
                 return turns
-            
+        
             for child in children(locks):
                 if child not in visit:
+                    queue.append((child, turns + 1))
                     visit.add((child))
-                    q.append((child, turns + 1))
-        
+                
         return -1
