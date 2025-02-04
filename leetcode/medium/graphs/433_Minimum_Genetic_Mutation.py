63608b115e9a3f14661b488989e39ec1a5fdca79
orchlonn <ch.orchlon@gmail.com>
2025-02-04 06:53:59 -0800

    433-spaced repetition

--- leetcode/medium/graphs/433_Minimum_Genetic_Mutation.py
+++ leetcode/medium/graphs/433_Minimum_Genetic_Mutation.py
@@ -1,18 +1,19 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        queue = deque([(startGene, 0)])
-        visit = set((startGene))
-        
+        queue, visit = deque(), set()
+        queue.append((startGene, 0))
+        visit.add((startGene))       
+
         while queue:
-            string, steps = queue.popleft()
-            if string == endGene:
-                return steps
+            node, turns = queue.popleft()
+            if node == endGene:
+                return turns
             
             for c in "ACGT":
-                for i in range(len(string)):
-                    neighbor = string[:i] + c + string[i + 1:]
-                    if neighbor not in visit and neighbor in bank:
-                        queue.append((neighbor, steps + 1))
-                        visit.add((neighbor))
-                        
+                for i in range(len(node)):
+                    digit = node[:i] + c + node[i+1:]
+                    if digit not in visit and digit in bank:
+                        queue.append((digit, turns + 1))
+                        visit.add((digit))
+
         return -1
