38a869b35a7e0e38749a03bb96954e4d6fb2d8d5
orchlonn <ch.orchlon@gmail.com>
2025-02-05 06:02:03 -0800

    1306-spaced repetition

--- leetcode/medium/graphs/1306_Jump_Game_III.py
+++ leetcode/medium/graphs/1306_Jump_Game_III.py
@@ -1,20 +1,21 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        queue = [start]
-        visit = set()
+        queue, visit = deque(), set()
+        queue.append((start))
 
         while queue:
-            pos = queue.pop()
-            jump = arr[pos]
+            pos = queue.popleft()
 
-            if jump == 0:
+            if arr[pos] == 0:
                 return True
-            
+
             if pos not in visit:
                 visit.add((pos))
-                if pos + jump < len(arr):
-                    queue.append(pos + jump)
-                if pos - jump >= 0:
-                    queue.append(pos - jump)
-            
+
+                if pos + arr[pos] < len(arr):
+                    queue.append((pos + arr[pos]))
+
+                if pos - arr[pos] >= 0:
+                    queue.append((pos - arr[pos]))
+
         return False
