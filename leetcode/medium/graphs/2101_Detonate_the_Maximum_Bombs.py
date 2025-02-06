c809bb371329771cc94d6d3cd6f33fdfb2464349
orchlonn <ch.orchlon@gmail.com>
2025-02-06 05:53:16 -0800

    2101-spaced repetition

--- leetcode/medium/graphs/2101_Detonate_the_Maximum_Bombs.py
+++ leetcode/medium/graphs/2101_Detonate_the_Maximum_Bombs.py
@@ -1,27 +1,28 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         graph = defaultdict(list)
-      
+        
         for i in range(len(bombs)):
             for j in range(i + 1, len(bombs)):
                 x1, y1, r1 = bombs[i]
                 x2, y2, r2 = bombs[j]
                 
-                dist = sqrt((x1 - x2)**2 + (y1 -y2)**2)
+                dist = sqrt((x1-x2)**2 + (y1-y2)**2)
                 
                 if dist <= r1:
                     graph[i].append(j)
                 if dist <= r2:
                     graph[j].append(i)
-                    
+        
         def dfs(i, visit):
             if i in visit:
                 return 0
-            visit.add(i)
+            
+            visit.add((i))
             for nei in graph[i]:
                 dfs(nei, visit)
             return len(visit)
-        
+            
         res = 0
         for i in range(len(bombs)):
             res = max(res, dfs(i, set()))
