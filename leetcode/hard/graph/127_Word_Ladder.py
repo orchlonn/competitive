aa1eb46b1241c776589dd4be0bc3ab2a6ca649ab
orchlonn <ch.orchlon@gmail.com>
2025-02-07 06:11:48 -0800

    127-spaced repetition

--- leetcode/hard/graph/127_Word_Ladder.py
+++ leetcode/hard/graph/127_Word_Ladder.py
@@ -1,34 +1,35 @@
-from collections import defaultdict, deque
-from typing import List
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
         
-        nei = defaultdict(list)
+        graph = defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[:i] + "*" + word[i + 1:]
-                nei[pattern].append(word)
-
+                pattern = word[:i] + "*" + word[i+1:]
+                graph[pattern].append(word)
         
-        queue = deque([beginWord])
-        visit = set([beginWord])
+        queue, visit = deque(), set()
+        queue.append((beginWord))
+        visit.add((beginWord))
         
-        res = 1  
+        res = 1
         while queue:
-            for _ in range(len(queue)):  
+            for _ in range(len(queue)):
                 word = queue.popleft()
+                
                 if word == endWord:
                     return res
                 
-                for i in range(len(word)):  
-                    pattern = word[:i] + "*" + word[i + 1:]
-                    for neighbor in nei[pattern]:
-                        if neighbor not in visit:
-                            visit.add(neighbor)
-                            queue.append(neighbor)
-            res += 1  
+                for i in range(len(word)):
+                    pattern = word[:i] + "*" + word[i+1:]
+                    for nei in graph[pattern]:
+                        if nei not in visit:
+                            queue.append((nei))
+                            visit.add((nei))
+            
+            res +=1
         
-        return 0  
+        return 0
+                    
+                    
