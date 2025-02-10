3a0776eecec9c9cd3990802f73eb976ccb36b267
orchlonn <ch.orchlon@gmail.com>
2025-02-10 14:52:18 -0800

    1615-spaced repetition

--- leetcode/medium/graphs/1615_Maximal_Network_Rank.py
+++ leetcode/medium/graphs/1615_Maximal_Network_Rank.py
@@ -1,15 +1,17 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for src, dest in roads:
-            graph[src].append(dest)
-            graph[dest].append(src)
+      graph = defaultdict(list)
+      
+      for i, j in roads:
+        graph[i].append(j)
+        graph[j].append(i)
+      
+      res = 0
+      for c1, c2 in combinations(graph.keys(), 2):
+        connection = 1 if c1 in graph[c2] else 0
+        c1_len = len(graph[c1])
+        c2_len = len(graph[c2])
+        res = max(res, c1_len + c2_len - connection)
+      
+      return res
         
-        res = 0
-        for c1, c2 in combinations(graph.keys(), 2):
-            connection = 1 if c1 in graph[c2] else 0
-            c1_connection = len(graph[c1])
-            c2_connection = len(graph[c2])
-            res = max(res, c1_connection + c2_connection - connection)
-
-        return res
