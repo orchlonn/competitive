0a611215929c4ef3820b41a1d7c619179b782f35
orchlonn <ch.orchlon@gmail.com>
2025-02-17 08:16:48 -0800

    230-spaced repetition

--- leetcode/medium/tree/230_Kth_Smallest_Element_in_BST.py
+++ leetcode/medium/tree/230_Kth_Smallest_Element_in_BST.py
@@ -1,21 +1,20 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        sorted_arr = []
-
-        def inorder(root):
+        ans = []
+        def dfs(root):
             if not root:
-                return
+                return 
             
-            inorder(root.left)
-            sorted_arr.append(root.val)
-            inorder(root.right)
-        
-        inorder(root)
+            dfs(root.left)
+            ans.append(root.val)
+            dfs(root.right)
 
-        return sorted_arr[k - 1]
+        dfs(root)
+
+        return ans[k - 1] if len(ans) > 0 else []
