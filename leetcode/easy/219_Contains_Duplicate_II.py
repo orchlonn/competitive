9e08cc266f9e711256c617a43756f0f89ad85c5f
orchlonn <ch.orchlon@gmail.com>
2025-02-17 08:03:42 -0800

    structure cleaned

--- leetcode/easy/219_Contains_Duplicate_II.py
+++
@@ -1,9 +0,0 @@
-def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-    dic = {}
-    for i, j in enumerate(nums):
-        if j in dic and i - dic[j] <= k:
-            return True
-        dic[j] = i
-        
-    return False
-
