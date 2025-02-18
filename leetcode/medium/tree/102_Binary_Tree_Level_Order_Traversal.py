d6dd4814423f40a06614181e1fcaec4096057051
orchlonn <ch.orchlon@gmail.com>
2025-02-18 05:53:39 -0800

    102-spaced repetition

--- leetcode/medium/tree/102_Binary_Tree_Level_Order_Traversal.py
+++ leetcode/medium/tree/102_Binary_Tree_Level_Order_Traversal.py
@@ -1,30 +1,27 @@
-  # Definition for a binary tree node.
-  # class TreeNode:
-  #     def __init__(self, val=0, left=None, right=None):
-  #         self.val = val
-  #         self.left = left
-  #         self.right = right
-  class Solution:
-      def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = deque()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = deque([(root)])
         ans = []
 
-        if root:
-          queue.append(root)
-
-        level = 0
         while queue:
-          val = []
-          for i in range(len(queue)):
-            cur = queue.popleft()
-            val.append(cur.val)
-
-            if cur.right:
-              queue.append(cur.right)
-            if cur.left:
-              queue.append(cur.left)
-          
-          level += 1
-          ans.append(val)
+            val = []
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                val.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            
+            ans.append(val)
 
         return ans
