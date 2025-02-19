3f71833f88eba5b27834383e6e79fcd1d0b783b4
orchlonn <ch.orchlon@gmail.com>
2025-02-19 07:38:24 -0800

    104-spaced repetition

--- leetcode/easy/trees/104_Maximum_Depth_of_Binary_Tree.py
+++ leetcode/easy/trees/104_Maximum_Depth_of_Binary_Tree.py
@@ -1,36 +1,15 @@
-# BFS solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-          return 0
-        level = 0
-        q = deque([root])
+            return 0
 
-        while q: 
-          for i in range(len(q)):
-            node = q.popleft()
-            if node.left:
-              q.append(node.left)
-            if node.right:
-              q.append(node.right)
-          
-          level += 1
-        
-      return level
-
-# DFS solution
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        stack = [[root, 1]]
-        res = 0
-
-        while stack:
-          node, depth = stack.pop()
-
-          if node:
-            stack.append([node.left, depth + 1])
-            stack.append([node.right, depth + 1])
-            res = max(res, depth)
-          
-        
-        return res
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        print(left, right)
+        return max(left, right) + 1
