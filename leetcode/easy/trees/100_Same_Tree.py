5d66d8eab70d4a00ebb7159745e07b257dcca44e
orchlonn <ch.orchlon@gmail.com>
2025-02-21 11:29:41 -0800

    100-spaced repetition

--- leetcode/easy/trees/100_Same_Tree.py
+++ leetcode/easy/trees/100_Same_Tree.py
@@ -6,18 +6,19 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None and  q == None:
+        if not p and not q:
             return True
-
-        if p == None or q == None:
+        
+        if not p or not q:
             return False
         
         if p.val != q.val:
             return False
-
-
+        
         left = self.isSameTree(p.left, q.left)
         right = self.isSameTree(p.right, q.right)
 
         return left and right
-            
+
+# Time complexity: O(n)
+# Space complexity: O(log n)
