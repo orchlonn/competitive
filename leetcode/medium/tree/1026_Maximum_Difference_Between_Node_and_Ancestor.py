5fa4cd1fb8a33ddca813b5062966c03b457a7634
orchlonn <ch.orchlon@gmail.com>
2025-02-23 07:38:01 -0800

    1026-spaced repetition

--- leetcode/medium/tree/1026_Maximum_Difference_Between_Node_and_Ancestor.py
+++ leetcode/medium/tree/1026_Maximum_Difference_Between_Node_and_Ancestor.py
@@ -1,18 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def maxAncestorDiff(self, root: TreeNode) -> int:
-        if not root:
-            return 0 
-        
-        def dfs(node, currMax, currMin):
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, minVal, maxVal):
             if not node:
-                return currMax - currMin
+                return maxVal - minVal
             
-            currMax = max(currMax, node.val)
-            currMin = min(currMin, node.val)
-
-            left = dfs(node.left, currMax, currMin)
-            right = dfs(node.right, currMax, currMin)
-
+            minVal = min(minVal, node.val)
+            maxVal = max(maxVal, node.val)
+            left = dfs(node.left, minVal, maxVal)
+            right = dfs(node.right, minVal, maxVal)
+            
             return max(left, right)
         
         return dfs(root, root.val, root.val)
+
+
+        # Time complexity: O(n)
+        # Space complexity: Worst - O(n)
+        #                   Best - O(logn)
