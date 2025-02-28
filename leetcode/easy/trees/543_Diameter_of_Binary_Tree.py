6e168c3c859bfb3146ef98461a4bd52d8a404549
orchlonn <ch.orchlon@gmail.com>
2025-02-28 07:31:43 -0800

    543-spaced repetition

--- leetcode/easy/trees/543_Diameter_of_Binary_Tree.py
+++ leetcode/easy/trees/543_Diameter_of_Binary_Tree.py
@@ -1,20 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        diameter = 0 
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
 
         def dfs(node):
             if not node:
                 return 0
-            
+
             left = dfs(node.left)
             right = dfs(node.right)
-
             nonlocal diameter
-
             diameter = max(diameter, left + right)
-
             return max(left, right) + 1
-        
-        dfs(root)
 
+        dfs(root)
         return diameter
+
+# Time complexity: O(n)
+# Space complexity: O(n)
