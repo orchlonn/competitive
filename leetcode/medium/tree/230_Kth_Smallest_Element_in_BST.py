14bbb670b08fd43d74afbd47f9c47a4bc204452a
orchlonn <ch.orchlon@gmail.com>
2025-03-04 06:51:22 -0800

    230-spaced repetition

--- leetcode/medium/tree/230_Kth_Smallest_Element_in_BST.py
+++ leetcode/medium/tree/230_Kth_Smallest_Element_in_BST.py
@@ -7,14 +7,18 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         ans = []
-        def dfs(root):
-            if not root:
-                return 
-            
-            dfs(root.left)
-            ans.append(root.val)
-            dfs(root.right)
 
+        def dfs(node):
+            nonlocal ans
+            if not node: return 0
+            left = dfs(node.left)
+            ans.append(node.val)
+            right = dfs(node.right)
+        
         dfs(root)
 
-        return ans[k - 1] if len(ans) > 0 else []
+        return ans[k-1]
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
