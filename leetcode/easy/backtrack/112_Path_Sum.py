441ac671ae852bb5ae078b56c6ad6743af90e2ab
orchlonn <ch.orchlon@gmail.com>
2025-03-08 08:10:05 -0800

    112-solved

---
+++ leetcode/easy/backtrack/112_Path_Sum.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curSum):
+            if not node:
+                return False
+            
+            curSum += node.val
+
+            if not node.left and not node.right:
+                return curSum == targetSum
+            
+            left = dfs(node.left, curSum)
+            right = dfs(node.right, curSum)
+
+            return left or right
+        
+        return dfs(root, 0)
+
+# Time complexity: O(n)
+# Space complexity: O(h)
+    # For skewed tree: O(n)
