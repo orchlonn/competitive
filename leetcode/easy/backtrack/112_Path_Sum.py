9c8fbe1df326173afb7bdee30b7494a9273dc083
orchlonn <ch.orchlon@gmail.com>
2025-03-09 08:54:03 -0700

    112-spaced repetition

--- leetcode/easy/backtrack/112_Path_Sum.py
+++ leetcode/easy/backtrack/112_Path_Sum.py
@@ -9,12 +9,10 @@ class Solution:
         def dfs(node, curSum):
             if not node:
                 return False
-            
             curSum += node.val
-
             if not node.left and not node.right:
                 return curSum == targetSum
-            
+
             left = dfs(node.left, curSum)
             right = dfs(node.right, curSum)
 
