2e71abab5dfd4adcce1af9223bf9eb26d7aa8967
orchlonn <ch.orchlon@gmail.com>
2025-03-10 09:11:25 -0700

    78-spaced repetition

--- leetcode/medium/backtrack/78_Subsets.py
+++ leetcode/medium/backtrack/78_Subsets.py
@@ -1,23 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-      res = []
-      subset = []
+        ans = []
+        subset = []
 
-      def dfs(index):
-        if index >= len(nums):
-          res.append(subset.copy())
-          return
+        def backtrack(index):
+            if index >= len(nums):
+                ans.append(subset.copy())
+                return
+            
+            # include nums[index]
+            subset.append(nums[index])
+            backtrack(index + 1)
+
+            # not include nums[index]
+            subset.pop()
+            backtrack(index + 1)
         
-        # include nums[index]
-        subset.append(nums[index])
-        dfs(index + 1)
-
-        # Not include nums[index]
-        subset.pop()
-        dfs(index + 1)
-      
-      dfs(0)
-      return res
+        backtrack(0)
+        return ans
 
 # Time complexity: O(2^n)
 # Space complexity: O(n * 2^n)
