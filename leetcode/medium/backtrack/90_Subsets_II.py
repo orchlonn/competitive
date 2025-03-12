9d5b45d8587fb161b47e69e5cd218597450808e7
orchlonn <ch.orchlon@gmail.com>
2025-03-12 06:40:51 -0700

    90-spaced repetition

--- leetcode/medium/backtrack/90_Subsets_II.py
+++ leetcode/medium/backtrack/90_Subsets_II.py
@@ -1,19 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        
-        def backtrack(comb, i):
-            if i > len(nums):
+        def backtrack(subsets, index, curSet):
+            if index >= len(nums):
+                subsets.append(curSet.copy())
                 return
-            
-            if list(comb) not in ans:
-                ans.append(list(comb))
 
-            for j in range(i, len(nums)):
-                comb.append(nums[j])
-                backtrack(comb, j + 1)
-                comb.pop()
+            # include nums[index]
+            curSet.append(nums[index])            
+            backtrack(subsets, index + 1, curSet)
+            curSet.pop()
 
-        ans = []
-        backtrack([], 0)
-        return ans
+            # not include nums[index]
+            while index + 1 < len(nums) and nums[index] == nums[index + 1]:
+                index += 1
+            backtrack(subsets, index + 1, curSet)
+
+
+        subsets, curSet = [], []
+        backtrack(subsets, 0, curSet)
+        return subsets
+
+# Time complexity: O(n * 2^n)
+# Space complexity: O(n) == O(h)
