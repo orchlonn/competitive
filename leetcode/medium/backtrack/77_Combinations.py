75532c6ba86d8c61ff410a746bc12b77f0e69806
orchlonn <ch.orchlon@gmail.com>
2025-03-14 07:17:14 -0700

    77-spaced repetition

--- leetcode/medium/backtrack/77_Combinations.py
+++ leetcode/medium/backtrack/77_Combinations.py
@@ -1,22 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def dfs(i, curComb):
+        def backtrack(i, curComb):
             if len(curComb) >= k:
-                comb.append(curComb.copy())
-                return 
-            
-            if i > n:
+                ans.append(curComb.copy())
                 return
             
             for j in range(i, n + 1):
                 curComb.append(j)
-                dfs(j + 1, curComb)
+                backtrack(j + 1, curComb)
                 curComb.pop()
 
-        comb = []
-        dfs(1, [])
-        return comb
-
+        ans = []
+        backtrack(i, [])
+        return ans
 
 # Time complexity: O(k * C(n, k)) 
 # Space complexity: O(k) + O(C(n,k))
