7f61daf1c869a2afe0ca7c90fcf36179c385893e
orchlonn <ch.orchlon@gmail.com>
2025-03-16 08:13:12 -0700

    46-spaced repetition

--- leetcode/medium/backtrack/46_Permutations.py
+++ leetcode/medium/backtrack/46_Permutations.py
@@ -1,16 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                ans.append(curr[:])
-                return
+        def backtrack(i):
+            if i == len(nums):
+                return [[]]
+            
+            resPerms = []
+            perms = backtrack(i + 1)
+            print(perms, i)
 
-            for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
-        
-        ans = []
-        backtrack([])
-        return ans
+            for p in perms:
+                for j in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(j, nums[i])
+                    resPerms.append(pCopy)
+            return resPerms
+
+        return backtrack(0)
+
+# Time complexity: O(n^2 * n!)
+# Space complexity: O(n^2 * n!)
