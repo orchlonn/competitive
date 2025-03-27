9c2eab1b088f11a52b9e5177a6b789b39b8777a5
orchlonn <ch.orchlon@gmail.com>
2025-03-27 09:21:07 -0700

    967-spaced repetition

--- leetcode/medium/backtrack/967_Numbers_With_Same_Consecutive_Differences.py
+++ leetcode/medium/backtrack/967_Numbers_With_Same_Consecutive_Differences.py
@@ -1,24 +1,23 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        def backtrack(n , num):
-            if n == 0:
+        def backtrack(N, num):
+            if N == 0:
                 ans.append(num)
                 return
-            
+
             tailDigit = num % 10
-            nextDigits = set([tailDigit - k, tailDigit + k])
+            nextDigits = set([tailDigit + k, tailDigit - k])
 
             for nextDigit in nextDigits:
                 if 0 <= nextDigit < 10:
                     newDigit = num * 10 + nextDigit
-                    backtrack(n - 1, newDigit)
+                    backtrack(N - 1, newDigit)
 
         ans = []
         for num in range(1, 10):
             backtrack(n - 1, num)
-
+        
         return ans
-
 
 # Time complexity: O(2^n)
 # Space complexity: O(2^n)
