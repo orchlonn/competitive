2749844907c4a21f9a5482be1e5a6781ac0aa2e5
orchlonn <ch.orchlon@gmail.com>
2025-04-10 11:42:09 -0700

    70-space complexity improvement

--- leetcode/easy/dynamic programming/70_Climbing_Stairs.py
+++ leetcode/easy/dynamic programming/70_Climbing_Stairs.py
@@ -12,7 +12,7 @@ class Solution:
 # Space complexity: O(N)
 
 
-#Solutoin 2: Using DP-top to bottom
+# Solutoin 2: Using DP-top to bottom
 class Solution:
     def climbStairs(self, n: int) -> int:
         def dfs(i, cache):
@@ -24,6 +24,21 @@ class Solution:
             cache[i] = dfs(i + 1, cache) + dfs(i + 2, cache)
             return cache[i]
         return dfs(0, {})
+
+# Time complexity: O(N)
+# Space complexity: O(1)
+
+
+# Solution 3: Using DP (Space optimized)
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
 
 # Time complexity: O(N)
 # Space complexity: O(1)
