c8351754ecd7e155dcf7a52ab116385bb65b1155
orchlonn <ch.orchlon@gmail.com>
2025-04-10 11:06:06 -0700

    509-efficient dp solution

--- leetcode/easy/dynamic programming/509_Fibonacci_Number.py
+++ leetcode/easy/dynamic programming/509_Fibonacci_Number.py
@@ -12,3 +12,25 @@ class Solution:
         return memoization(n, {})
 
 # Time complexity: O(N)
+# Space complexity: O(N)
+
+
+
+# Solution 2
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        
+        dp = [0, 1]
+        i = 2
+        while i <= n:
+            tmp = dp[1]
+            dp[1] = dp[1] + dp[0]
+            dp[0] = tmp
+            i += 1
+        
+        return dp[1]
+
+# Time complexity: O(N)
+# Space complexity: O(1)
