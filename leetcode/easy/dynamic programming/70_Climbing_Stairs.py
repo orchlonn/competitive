5759d73fd8ceb7550e011225acf72df3dc8dd4c6
orchlonn <ch.orchlon@gmail.com>
2025-04-10 11:35:49 -0700

    70-efficient dp solution

--- leetcode/easy/dynamic programming/70_Climbing_Stairs.py
+++ leetcode/easy/dynamic programming/70_Climbing_Stairs.py
@@ -10,3 +10,20 @@ class Solution:
 
 # Time complexity: O(N)
 # Space complexity: O(N)
+
+
+#Solutoin 2: Using DP-top to bottom
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def dfs(i, cache):
+            if i >= n:
+                return i == n
+            if i in cache:
+                return cache[i]
+
+            cache[i] = dfs(i + 1, cache) + dfs(i + 2, cache)
+            return cache[i]
+        return dfs(0, {})
+
+# Time complexity: O(N)
+# Space complexity: O(1)
