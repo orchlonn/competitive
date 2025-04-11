3b6611e1c7d193afd33d5ee332a8f9dce9ca74a7
orchlonn <ch.orchlon@gmail.com>
2025-04-11 07:13:47 -0700

    746-space optimized solution

--- leetcode/medium/dynamic programming/746_Min_Cost_Climbing_Stairs.py
+++ leetcode/medium/dynamic programming/746_Min_Cost_Climbing_Stairs.py
@@ -14,3 +14,20 @@ class Solution:
 
 # Time complexity: O(N)
 # Space complexity: O(N)
+
+
+# Solution 2: (Space optimized)
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0, 0]
+
+        for i in range(2, n + 1):
+            tmp = min(dp[1] + cost[i - 1], dp[0] + cost[i - 2])
+            dp[0] = dp[1]
+            dp[1] = tmp
+
+        return dp[1]
+
+# Time complexity: O(N)
+# Space complexity: O(1)
