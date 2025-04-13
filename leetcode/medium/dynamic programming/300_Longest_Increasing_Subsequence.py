d64d7525384817b576622f93ca8b2077ed7de1ab
orchlonn <ch.orchlon@gmail.com>
2025-04-13 08:42:55 -0700

    300-spaced repetition

--- leetcode/medium/dynamic programming/300_Longest_Increasing_Subsequence.py
+++ leetcode/medium/dynamic programming/300_Longest_Increasing_Subsequence.py
@@ -1,12 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
+        res = [1] * len(nums)
 
         for i in range(len(nums), -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[j] > nums[i]:
-                    LIS[i] = max(LIS[i], 1 + LIS[j])
-        
-        return max(LIS)
+                    res[i] = max(res[i], 1 + res[j])
+            
+        return max(res)
+
 
 # Time complexity: O(N^2)
+# Space complexity: O(N)
