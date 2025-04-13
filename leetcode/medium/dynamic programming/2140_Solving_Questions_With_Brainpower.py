5acfcf7303ee4f825f5d7a9c51a07d19070e13d4
orchlonn <ch.orchlon@gmail.com>
2025-04-13 09:49:54 -0700

    2140-bottom up solution

--- leetcode/medium/dynamic programming/2140_Solving_Questions_With_Brainpower.py
+++ leetcode/medium/dynamic programming/2140_Solving_Questions_With_Brainpower.py
@@ -22,3 +22,25 @@ class Solution:
 
 # Time complexity: O(N)
 # Space complexity: O(N) = [cache + recursion stack]
+
+
+
+# Soltion #2 (Bottom up)
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        N = len(questions)
+        cache = {}
+
+        for i in reversed(range(N)):
+            points, brainpower = questions[i]
+            next_i = i + 1 + brainpower 
+
+            choose = points + (cache[next_i] if next_i < N else 0)
+            skip = cache[i + 1] if i + 1 < N else 0
+            
+            cache[i] = max(choose, skip)
+    
+        return cache[0]
+
+# Time complexity: O(N)
+# Space complexity: O(N) = [only cache]
