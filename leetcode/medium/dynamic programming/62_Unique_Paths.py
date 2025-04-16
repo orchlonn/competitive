e8121950e0e52550c61352a11fbcb6a84e5493fb
orchlonn <ch.orchlon@gmail.com>
2025-04-16 07:26:34 -0700

    62-spaced repetition

--- leetcode/medium/dynamic programming/62_Unique_Paths.py
+++ leetcode/medium/dynamic programming/62_Unique_Paths.py
@@ -21,16 +21,14 @@ class Solution:
 # Solution #2 (Bottom up)
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        prevRow = [0] * n
+        dp = [0] * n
 
         for r in range(m - 1, -1, -1):
-            curRow = [0] * n
-            curRow[-1] = 1 
+            curRow = [1] * n
             for c in range(n - 2, -1, -1):
-                curRow[c] = curRow[c + 1] + prevRow[c]
-            prevRow = curRow
-        
-        return prevRow[0]
+                curRow[c] = curRow[c + 1] + dp[c]
+            dp = curRow
+        return dp[0]
 
 # Time complexity: O(M * N)
 # Space complexity: O(N)
