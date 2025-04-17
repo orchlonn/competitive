cbdd01dd29b584ef8aa657cd3e7050c4a6257127
orchlonn <ch.orchlon@gmail.com>
2025-04-17 07:56:12 -0700

    63-top down solution added

--- leetcode/medium/dynamic programming/63_Unique_Paths_II.py
+++ leetcode/medium/dynamic programming/63_Unique_Paths_II.py
@@ -1,3 +1,4 @@
+# Solution 1: Bottom up
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
@@ -15,3 +16,26 @@ class Solution:
 
 # Time complexity: O(COLS * ROWS)
 # Space complexity: O(COLS)
+
+
+# Solution 2: Top down
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
+        dp = {(ROWS - 1, COLS - 1): 1}
+
+        def dfs(r, c):
+            if r == ROWS or c == COLS or obstacleGrid[r][c] == 1:
+                return 0
+            
+            if (r, c) in dp:
+                return dp[(r, c)]
+            
+            dp[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
+            return dp[(r, c)]
+        return dfs(0, 0)
+
+# Time complexity: O(COLS * ROWS)
+# Space complexity: O(COLS)
+
+
