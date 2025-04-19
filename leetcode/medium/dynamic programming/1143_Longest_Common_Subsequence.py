344dbb818a5b67ac57cc664eb1a44465bb5d1948
orchlonn <ch.orchlon@gmail.com>
2025-04-19 06:55:11 -0700

    1143-spaced repetition

--- leetcode/medium/dynamic programming/1143_Longest_Common_Subsequence.py
+++ leetcode/medium/dynamic programming/1143_Longest_Common_Subsequence.py
@@ -2,16 +2,16 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         M, N = len(text1), len(text2)
         dp = [[0] * (N + 1) for i in range(M + 1)]
+        dp[M - 1][N - 1] = 1
 
-        for i in reversed(range(M)):
-            for j in reversed(range(N)):
-                if text1[i] == text2[j]:
-                    print(i, j)
-                    dp[i][j] += dp[i + 1][j + 1] + 1
-                else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
-    
+        for r in reversed(range(M)):
+            for c in reversed(range(N)):
+                if text1[r] == text2[c]:
+                    dp[r][c] = dp[r + 1][c + 1] + 1
+                elif c < N:
+                    dp[r][c] = max(dp[r + 1][c], dp[r][c + 1])
+
         return dp[0][0]
 
-# Time complexity: O(M * N)
-# Space complexity: O(M * N)
+# Time complexity: O(N * M)
+# Space complexity: O(N * M)
