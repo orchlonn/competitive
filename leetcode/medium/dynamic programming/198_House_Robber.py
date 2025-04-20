6adb0b92cba6e2be1f69a5af1267bb77f67a2699
orchlonn <ch.orchlon@gmail.com>
2025-04-20 07:43:31 -0700

    198-added optimized solution

--- leetcode/medium/dynamic programming/198_House_Robber.py
+++ leetcode/medium/dynamic programming/198_House_Robber.py
@@ -1,5 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            
+            if i in cache: 
+                return cache[i]
+            
+            cache[i] = max(dp(i - 1), dp(i - 2) + nums[i])
+            return cache[i]
+
+        cache = {}
+        return dp(len(nums) - 1)
+
+# Time complexity: O(N)
+# Spcae complexity: O(N)
+
+# Solution 2 (space optimized solution)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
 
         for num in nums:
