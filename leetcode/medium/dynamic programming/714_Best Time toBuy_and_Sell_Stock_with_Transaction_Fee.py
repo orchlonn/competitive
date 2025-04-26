86b8833c64a4a4f6664bf1b3728f24c5dc456743
orchlonn <ch.orchlon@gmail.com>
2025-04-26 07:23:40 -0700

    714-spaced repetition

--- leetcode/medium/dynamic programming/714_Best Time toBuy_and_Sell_Stock_with_Transaction_Fee.py
+++ leetcode/medium/dynamic programming/714_Best Time toBuy_and_Sell_Stock_with_Transaction_Fee.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        hold, free = [0] * n, [0] * n
-
-        hold[0] = -prices[0]
+        free, hold = [0] * n, [0] * n
         
+        hold[0] = -prices[0]
+
         for i in range(1, n):
-            hold = max(hold[i - 1], free[i - 1] - prices[i])
-            free = max(free[i - 1], hold[i - 1] + prices[i] - fee)
+            hold[i] = max(hold[i - 1], free[i - 1] - prices[i])
+            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee)
 
         return free[-1]
