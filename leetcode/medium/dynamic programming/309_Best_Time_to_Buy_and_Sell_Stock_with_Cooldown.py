ac9e52fb32db645911c3a16a2b3af34497fe43b6
orchlonn <ch.orchlon@gmail.com>
2025-04-27 09:01:19 -0700

    309-spaced repetition

--- leetcode/medium/dynamic programming/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
+++ leetcode/medium/dynamic programming/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
@@ -1,24 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
+
+        cache = {}
 
         def dfs(i, buying):
             if i >= len(prices):
                 return 0
-            
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-            
-            cooldown = dfs(i + 1, buying)
+
+            if (i, buying) in cache:
+                return cache[(i, buying)]
+
             if buying:
                 hold = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(hold, cooldown)
+                cooldown = dfs(i + 1, buying)
+                cache[(i, buying)] = max(hold, cooldown)
             else:
                 sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-
-            return dp[(i, buying)]
-
+                cooldown = dfs(i + 1, buying)
+                cache[(i, buying)] = max(sell, cooldown)
+            
+            return cache[(i, buying)]
+        
         return dfs(0, True)
 
-# Time complexity: O(N)
+
+# Time complexity: O(n)
