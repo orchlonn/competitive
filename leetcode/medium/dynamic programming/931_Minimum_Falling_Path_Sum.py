11ad7fbc87b23b65eee37d7a9961e8372a4e86bd
orchlonn <ch.orchlon@gmail.com>
2025-04-30 12:35:21 -0700

    931-spaced repetition

--- leetcode/medium/dynamic programming/931_Minimum_Falling_Path_Sum.py
+++ leetcode/medium/dynamic programming/931_Minimum_Falling_Path_Sum.py
@@ -1,32 +1,31 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        N = len(matrix)
         cache = {}
+        N = len(matrix)
 
         def dfs(r, c):
             if r == N:
                 return 0
             
-            if c < 0 or c == N:
+            if c == N or c < 0:
                 return float('inf')
-
+            
             if (r, c) in cache:
                 return cache[(r, c)]
-
-            ans = matrix[r][c] + min(
+            
+            res = matrix[r][c] + min(
                 dfs(r + 1, c - 1),
                 dfs(r + 1, c),
-                dfs(r + 1, c + 1),
+                dfs(r + 1, c + 1)
             )
-
-            cache[(r, c)] = ans
-            return ans
+            cache[(r, c)] = res
+            return res
         
-        res = float('inf')
+        ans = float('inf')
         for c in range(N):
-            res = min(res, dfs(0, c))
+            ans = min(ans, dfs(0, c))
         
-        return res
+        return ans
 
 # Time complexity: O(N^2)
 # Space complexity: O(N^2)
