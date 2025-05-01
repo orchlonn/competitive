f6e82ccd5ef579915984aa154a06ba32b541466b
orchlonn <ch.orchlon@gmail.com>
2025-05-01 09:55:11 -0700

    494-space optimized solution added

--- leetcode/medium/dynamic programming/494_Target_Sum.py
+++ leetcode/medium/dynamic programming/494_Target_Sum.py
@@ -1,10 +1,29 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+
+        for i in range(len(nums)):
+            next_dp = dp
+            for curr_sum, count in dp:
+                next_dp[curr_sum + nums[i]] += count
+                next_dp[curr_sum - nums[i]] += count
+            dp = next_dp
+        
+        return dp[target]
+
+# Time complexity: O(N * T) where t = sum(nums)
+# Space complexity: O(T)
+
+# Solution #2: Top down
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         ans = 0
         cache = {}
+        N = len(nums)
 
         def dfs(i, curSum):
-            if i == len(nums):
+            if i == N:
                 return 1 if curSum == target else 0
 
             if (i, curSum) in cache:
@@ -18,3 +37,6 @@ class Solution:
             return cache[(i, curSum)]
 
         return dfs(0, 0)
+
+# Time complexity: O(N * T) where t = sum(nums)
+# Space complexity: O(N * T)
