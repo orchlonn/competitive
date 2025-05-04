0c90c0fd9a23b1e4a8bc0e1e43181829f645ed8d
orchlonn <ch.orchlon@gmail.com>
2025-05-04 09:00:18 -0700

    1035-time complexity optimized

--- leetcode/medium/dynamic programming/1035_Uncrossed_Lines.py
+++ leetcode/medium/dynamic programming/1035_Uncrossed_Lines.py
@@ -19,3 +19,21 @@ class Solution:
 
 # Time complexity: O(N * M)
 # Space complexity: O(N * M)
+
+
+# Solution #2:
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        
+        return dp[-1][-1]
+        
+# Time complexity: O(N * M)
+# Space complexity: O(N * M)
