c8bdb55cc15ed7c9480f07388bb604adfe77750b
orchlonn <ch.orchlon@gmail.com>
2025-05-07 07:41:56 -0700

    bug fixed

--- leetcode/medium/dynamic programming/139_Word_Break.py
+++ leetcode/medium/dynamic programming/139_Word_Break.py
@@ -22,7 +22,7 @@ class Solution:
         dp = [False] * (len(s) + 1)
         dp[-1] = True
 
-        for i in reversed(range(len(s) - 1)):
+        for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
                 if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
                     dp[i] = dp[i + len(w)]
