c68cd0699a034189345f5bbd825c024290426300
orchlonn <ch.orchlon@gmail.com>
2025-05-07 07:40:13 -0700

    139-bottom up solution

--- leetcode/medium/dynamic programming/139_Word_Break.py
+++ leetcode/medium/dynamic programming/139_Word_Break.py
@@ -14,3 +14,28 @@ class Solution:
             return False
         
         return dfs(0)
+
+
+# Solution #2: (Bottom up):
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+
+        for i in reversed(range(len(s) - 1)):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+        
+        return dp[0]
+
+
+# Time complexity: O(N * M * K)
+# Space complexity: O(N)
+
+
+# n: Length of the input string s.
+# m: Number of words in wordDict.
+# k: Average length of words in wordDict.
