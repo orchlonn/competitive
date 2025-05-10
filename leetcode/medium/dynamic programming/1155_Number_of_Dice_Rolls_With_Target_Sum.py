9264d8c1ee4e89f60eda45f99a42678c2edd6584
orchlonn <ch.orchlon@gmail.com>
2025-05-10 07:54:16 -0700

    1155-bottom up solution added

--- leetcode/medium/dynamic programming/1155_Number_of_Dice_Rolls_With_Target_Sum.py
+++ leetcode/medium/dynamic programming/1155_Number_of_Dice_Rolls_With_Target_Sum.py
@@ -24,3 +24,25 @@ class Solution:
 
 # Time complexity: O(N * K * Target)
 # Space complexity: O(N * Target)
+
+
+# Solution #2 
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        MOD = 10**9 + 7
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for dice in range(n):
+            next_dp = [0] * (target + 1)
+
+            for val in range(1, k + 1):
+                for total in range(val, target + 1):
+                    next_dp[total] = (next_dp[total] + dp[total - val]) % MOD
+            
+            dp = next_dp
+        
+        return dp[-1]
+
+# Time complexity: O(N * K * Target)
+# Space complexity: O(Target)
