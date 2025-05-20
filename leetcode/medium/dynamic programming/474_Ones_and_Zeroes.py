4b0f02140eec75b84a7950403bfe2bf264992e7f
orchlonn <ch.orchlon@gmail.com>
2025-05-20 07:11:08 -0700

    474-spaced repetition

--- leetcode/medium/dynamic programming/474_Ones_and_Zeroes.py
+++ leetcode/medium/dynamic programming/474_Ones_and_Zeroes.py
@@ -1,29 +1,32 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        def count_binary(s):
-            zeros = s.count('0')
-            ones = s.count('1')
-            return zeros, ones
-
-        def dfs(i, m, n):
-            if i == len(strs):
+        def dfs(i, one_sum, zero_sum):
+            if i >= len(strs):
                 return 0
             
-            if (i, m, n) in cache:
-                return cache[(i, m, n)]
+            if (i, one_sum, zero_sum) in cache:
+                return cache[(i, one_sum, zero_sum)]
             
-            zeros, ones = count_binary(strs[i])
+            ones = strs[i].count('1')
+            zeros = strs[i].count('0')
+            
+            take = 0
+            if ones + one_sum <= n and zeros + zero_sum <= m:
+                # include element i
+                take = 1 + dfs(i + 1, ones + one_sum, zeros + zero_sum)
+            # skip el i
+            skip = dfs(i + 1, one_sum, zero_sum)
 
-            cache[(i, m, n)] = dfs(i + 1, m, n)
-
-            if zeros <= m and ones <= n:
-                cache[(i, m, n)] = max(
-                    cache[(i, m, n)],
-                    1 + dfs(i + 1, m - zeros, n - ones))
-            return cache[(i, m, n)]
-        
+            cache[(i, one_sum, zero_sum)] = max(take, skip)
+            return cache[(i, one_sum, zero_sum)]
+            
         cache = {}
-        return dfs(0, m, n)
+        return dfs(0, 0, 0)
 
-# Time complexity: O(k * m * n)
-# Space complexity: O(k * m * n)
+# | Complexity Type  | Value        |
+# | ---------------- | ------------ |
+# | Time Complexity  | O(L * m * n) |
+# | Space Complexity | O(L * m * n) |
+
+#  Because for each state (i, one_sum, zero_sum) you do constant work (either take or skip).
+
