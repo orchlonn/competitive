28ca41564794176a0d0c7b1cc4295a3bbc0c4769
orchlonn <ch.orchlon@gmail.com>
2025-05-23 14:45:19 -0700

    518-spaced repetition

--- leetcode/medium/dynamic programming/518_Coin_Change_II.py
+++ leetcode/medium/dynamic programming/518_Coin_Change_II.py
@@ -3,18 +3,21 @@ class Solution:
         def dfs(i, curSum):
             if curSum == amount:
                 return 1
+            
             if curSum > amount:
                 return 0
+                
             if i >= len(coins):
                 return 0
+
             if (i, curSum) in cache:
                 return cache[(i, curSum)]
-
-            cache[(i, curSum)] = dfs(i, curSum + coins[i]) + dfs(i + 1, curSum)
+            
+            cache[(i, curSum)] = (dfs(i, curSum + coins[i]) + dfs(i + 1, curSum))
             return cache[(i, curSum)]
-
+            
         cache = {}
         return dfs(0, 0)
 
-# Time complexity: O(n * amount)
-# Space complexity: O(n * amount)
+# Time complexity: O(N * amount)
+# Space complexity: O(N * amount) where N is coins
