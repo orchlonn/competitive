3d4b2aa5b584b2185f7942bbffcd7661cb0234c3
orchlonn <ch.orchlon@gmail.com>
2025-05-24 08:31:46 -0700

    1143-bottom up solution with optimized version added

--- leetcode/medium/dynamic programming/1143_Longest_Common_Subsequence.py
+++ leetcode/medium/dynamic programming/1143_Longest_Common_Subsequence.py
@@ -1,16 +1,63 @@
+# Solution 1:
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        M, N = len(text1), len(text2)
-        dp = [[0] * (N + 1) for i in range(M + 1)]
+        def dfs(i1, i2):
+            if i1 == len(text1) or i2 == len(text2):
+                return 0
+            
+            if (i1, i2) in cache:
+                return cache[(i1, i2)]
 
-        for i in reversed(range(M)):
-            for j in reversed(range(N)):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
-                else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+            if text1[i1] == text2[i2]:
+                cache[(i1, i2)] =  1 + dfs(i1 + 1, i2 + 1)
+            else:
+                cache[(i1, i2)] = max( dfs(i1 + 1, i2), 
+                            dfs(i1, i2 + 1))
 
-        return dp[0][0]
+            return cache[(i1, i2)]
+
+        cache = {}
+        return dfs(0, 0)
         
 # Time complexity: O(N * M)
 # Space complexity: O(N * M)
+
+
+# Soltution 2: (Bottom up - True dp solution)
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n):
+            for j in range(m):
+                if text1[i] == text2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        
+        return dp[-1][-1]
+
+# Time complexity: O(N * M)
+# Space complexity: O(N * M)
+
+# Solution 3: (Optimized bottom up solution)
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if len(text1) < len(text2): text1, text2 = text2, text1
+        N, M = len(text1), len(text2)
+        dp = [0] * (M + 1)
+
+        for i in range(N):
+            curRow = [0] * (M + 1)
+            for j in range(M):
+                if text1[i] == text2[j]:
+                    curRow[j + 1] = 1 + dp[j]
+                else:
+                    curRow[j + 1] = max(dp[j + 1], curRow[j])
+            dp = curRow
+
+        return dp[-1]
+
+# Time complexity: O(N * M)
+# Space complexity: O(min(N,M))
