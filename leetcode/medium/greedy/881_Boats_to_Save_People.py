b9645b518dd54b2549a6fcc92b110aa167f30436
orchlonn <ch.orchlon@gmail.com>
2025-06-05 07:37:11 -0700

    881-spaced repetition

--- leetcode/medium/greedy/881_Boats_to_Save_People.py
+++ leetcode/medium/greedy/881_Boats_to_Save_People.py
@@ -1,17 +1,16 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        i = 0
+        ans = i = 0
         j = len(people) - 1
-        ans = 0
         people.sort()
-
-        while j >= i:
+        
+        while i <= j:
             if people[i] + people[j] <= limit:
                 i += 1
             
             ans += 1
             j -= 1
-        
+ 
         return ans
         
 # Time complexity: O(n * log n)
