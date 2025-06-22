9b12be9377b7b9d368ff1e59d70fd1a281d25878
orchlonn <ch.orchlon@gmail.com>
2025-06-22 08:54:24 -0700

    560-time complexity optimized

--- leetcode/medium/array/560_Subarray_Sum_Equals_K.py
+++ leetcode/medium/array/560_Subarray_Sum_Equals_K.py
@@ -12,3 +12,23 @@ class Solution:
 
 # Time complexity: O(N^2)
 # Space complexity: O(1)
+
+
+# Solution #2:
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        curSum = 0
+        prefixSums = {0 : 1}
+
+        for n in nums:
+            curSum += n
+            diff = curSum - k
+
+            res += prefixSums.get(diff, 0)
+            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+        
+        return res
+
+# Time complexity: O(N)
+# Space complexity: O(N)
