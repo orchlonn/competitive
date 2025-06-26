cefdee215c9dc51e7cdcf53a988c11e5a360fd28
orchlonn <ch.orchlon@gmail.com>
2025-06-26 09:48:48 -0700

    141-spaced repetition

--- leetcode/easy/Linked List/141_Linked_List_Cycle.py
+++ leetcode/easy/Linked List/141_Linked_List_Cycle.py
@@ -1,13 +1,20 @@
 # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slower, faster = head, head
+        slow, fast = head, head
 
-        while faster and faster.next:
-            faster = faster.next.next
-            slower = slower.next
-            if slower == faster:
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
         
         return False
-        
+
+# Time complexity: O(N)
+# Space complexity: O(1)
