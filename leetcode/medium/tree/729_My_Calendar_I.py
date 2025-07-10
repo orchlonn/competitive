42b323dab882bdc4693ebe54ef977c42a89659af
orchlonn <ch.orchlon@gmail.com>
2025-07-10 09:27:22 -0700

    729-time complexity optimized

--- leetcode/medium/tree/729_My_Calendar_I.py
+++ leetcode/medium/tree/729_My_Calendar_I.py
@@ -1,3 +1,4 @@
+# Solution 1:
 class MyCalendar:
 
     def __init__(self):
@@ -10,6 +11,46 @@ class MyCalendar:
             
         self.events.append([startTime, endTime])
         return True
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
+
+
+# Solution 2:
+class Tree:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+
+    def insert(self, start, end):
+        curr = self
+        while True:
+            if start >= curr.end:
+                if not curr.right:
+                    curr.right = Tree(start, end)
+                    return True
+                curr = curr.right
+            elif end <= curr.start:
+                if not curr.left:
+                    curr.left = Tree(start, end)
+                    return True
+                curr = curr.left
+            else:
+                return False
+
+class MyCalendar:
+
+    def __init__(self):
+        self.root = None
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        if not self.root:
+            self.root = Tree(startTime, endTime)
+            return True
+        return self.root.insert(startTime, endTime)
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
