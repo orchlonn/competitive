7ce6449c8baf025f941b37a66082e910b69ec656
orchlonn <ch.orchlon@gmail.com>
2025-07-21 10:09:24 -0700

    1584-spaced repetition

--- leetcode/medium/graphs/1584_Min_Cost_to_Connect_All_Points.py
+++ leetcode/medium/graphs/1584_Min_Cost_to_Connect_All_Points.py
@@ -1,7 +1,8 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         N = len(points)
-        adj = {i: [] for i in range(N)}
+        adj = defaultdict(list)
+
         for i in range(N):
             x1, y1 = points[i]
             for j in range(i + 1, N):
@@ -9,17 +10,24 @@ class Solution:
                 dist = abs(x1 - x2) + abs(y1 - y2)
                 adj[i].append([dist, j])
                 adj[j].append([dist, i])
-
-        res = 0
+        
+        res = 0 
+        minHeap = [[0, 0]]
         visit = set()
-        minH = [[0, 0]]
-        while len(visit) < N:
-            cost, i = heapq.heappop(minH)
-            if i in visit:
+
+        while minHeap and len(visit) < N:
+            cost, node = heapq.heappop(minHeap)
+            if node in visit:
                 continue
+                
+            visit.add(node)
             res += cost
-            visit.add(i)
-            for neiCost, nei in adj[i]:
+
+            for neiCost, nei in adj[node]:
                 if nei not in visit:
-                    heapq.heappush(minH, [neiCost, nei])
+                    heapq.heappush(minHeap, [neiCost, nei])                
+            
         return res
+
+# Time complexity: O(N^2 * log(N))
+# Space complexity: O(N^2)
