0cfeb8bd8afb0308f733a44672a5ce95cae944bc
orchlonn <ch.orchlon@gmail.com>
2025-07-24 11:21:53 -0700

    210-alternative solution added

--- leetcode/medium/graphs/210_Course_Schedule_II.py
+++ leetcode/medium/graphs/210_Course_Schedule_II.py
@@ -30,3 +30,42 @@ class Solution:
         return output
 
 # Time complexity: O(P + N) where P is number of prerequisites and N is numCourse
+
+# Solution #2: Topological sort (DFS)
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+        
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        
+        visited = set()
+        visiting = set()
+        topSort = []
+
+        def dfs(crs):
+            if crs in visited:
+                return True
+            if crs in visiting:
+                return False
+            
+            visiting.add(crs)
+            for nei in adj[crs]:
+                if not dfs(nei):
+                    return False
+            
+            visiting.remove(crs)
+            visited.add(crs)
+            topSort.append(crs)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return topSort
+
+# Time Complexity: O(V + E)
+# Space Complexity: O(V + E)
