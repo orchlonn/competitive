a1f817612b99330ad87054bc541fdc27bca4cf50
orchlonn <ch.orchlon@gmail.com>
2025-07-24 11:18:02 -0700

    207-alternative solution added

--- leetcode/medium/graphs/207_Course_Schedule.py
+++ leetcode/medium/graphs/207_Course_Schedule.py
@@ -1,3 +1,4 @@
+# Solution 1: Cycle detection (DFS):
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         prereq = {c:[] for c in range(numCourses)}
@@ -25,3 +26,42 @@ class Solution:
                 return False
         
         return True
+
+
+# Topological Sort (DFS):
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        
+        visited = set()
+        visiting = set()
+        
+        def dfs(crs):
+            if crs in visited:
+                return True
+            if crs in visiting:
+                return False
+            
+            visiting.add(crs)
+
+            for nei in adj[crs]:
+                if not dfs(nei):
+                    return False
+            
+            visited.add(crs)
+            visiting.remove(crs)
+
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True
+
+# Time Complexity: O(V + E)
+# Space Complexity: O(V + E)
+
+
