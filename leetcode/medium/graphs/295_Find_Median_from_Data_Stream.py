cf8dc466b9d1652e8a37a2edd77a936ac00f94e3
orchlonn <ch.orchlon@gmail.com>
2025-07-27 11:15:25 -0700

    295-solved

---
+++ leetcode/medium/graphs/295_Find_Median_from_Data_Stream.py
@@ -0,0 +1,31 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.large, self.small = [], []
+
+    def addNum(self, num: int) -> None:
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num)
+        
+        # handle uneven size
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        elif len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return (-1 * self.small[0] + self.large[0]) / 2.0
+
+# Time complexity: O(M * log N)
+# Space complexity: O(N)
+# Where m is the number of function calls and n is the length of the array.
